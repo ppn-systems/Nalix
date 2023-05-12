ad09af89f58f2f74f9d66b38165f6085b72cdf8f
phucnguyen <nguyen098xx@gmail.com>
2023-05-12 19:09:28 +0700

    1.1.6

---
+++ src/Nalix.Common/Package/Metadata/PacketOffset.cs
@@ -0,0 +1,52 @@
+namespace Nalix.Common.Package.Metadata;
+
+/// <summary>
+/// Offsets for the components in the packet header.
+/// </summary>
+public static class PacketOffset
+{
+    /// <summary>
+    /// Offset for the packet length.
+    /// </summary>
+    public const System.Int32 Length = 0;
+
+    /// <summary>
+    /// Offset for the packet command.
+    /// </summary>
+    public const System.Int32 OpCode = Length + PacketSize.Length;
+
+    /// <summary>
+    /// Offset for the packet Number.
+    /// </summary>
+    public const System.Int32 Number = OpCode + PacketSize.OpCode;
+
+    /// <summary>
+    /// Offset for the packet checksum.
+    /// </summary>
+    public const System.Int32 Checksum = Number + PacketSize.Number;
+
+    /// <summary>
+    /// Offset for the packet timestamp.
+    /// </summary>
+    public const System.Int32 Timestamp = Checksum + PacketSize.Checksum;
+
+    /// <summary>
+    /// Offset for the packet type.
+    /// </summary>
+    public const System.Int32 Type = Number + PacketSize.Number;
+
+    /// <summary>
+    /// Offset for the packet flags.
+    /// </summary>
+    public const System.Int32 Flags = Type + PacketSize.Type;
+
+    /// <summary>
+    /// Offset for the packet priority.
+    /// </summary>
+    public const System.Int32 Priority = Flags + PacketSize.Flags;
+
+    /// <summary>
+    /// Offset for the packet payload.
+    /// </summary>
+    public const System.Int32 Payload = Priority + PacketSize.Priority;
+}
