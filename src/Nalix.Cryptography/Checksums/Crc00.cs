61443d60ec91ec4358028dc2d4eef31cd2ea7063
phucnguyen <nguyen098xx@gmail.com>
2023-06-03 22:46:08 +0700

    1.4.4

--- src/Nalix.Cryptography/Checksums/Crc00.cs
+++ src/Nalix.Cryptography/Checksums/Crc00.cs
@@ -8,15 +8,15 @@ internal static class Crc00
     /// <summary>
     /// Generates a lookup table for CRC-8/MODBUS.
     /// </summary>
-    internal static byte[] GenerateTable8(byte poly)
+    internal static System.Byte[] GenerateTable8(System.Byte poly)
     {
-        byte[] table = new byte[256];
+        System.Byte[] table = new System.Byte[256];
 
-        for (int i = 0; i < 256; i++)
+        for (System.Int16 i = 0; i < 256; i++)
         {
-            byte crc = (byte)i;
-            for (int j = 0; j < 8; j++)
-                crc = (byte)((crc & 0x80) != 0 ? (crc << 1) ^ poly : crc << 1);
+            System.Byte crc = (System.Byte)i;
+            for (System.Byte j = 0; j < 8; j++)
+                crc = (System.Byte)((crc & 0x80) != 0 ? (crc << 1) ^ poly : crc << 1);
 
             table[i] = crc;
         }
@@ -27,15 +27,15 @@ internal static class Crc00
     /// <summary>
     /// Generates a lookup table for CRC-16/MODBUS.
     /// </summary>
-    internal static ushort[] GenerateTable16(ushort poly)
+    internal static System.UInt16[] GenerateTable16(System.UInt16 poly)
     {
-        ushort[] table = new ushort[256];
+        System.UInt16[] table = new System.UInt16[256];
 
-        for (int i = 0; i < 256; i++)
+        for (System.Int16 i = 0; i < 256; i++)
         {
-            ushort crc = (ushort)i;
-            for (int j = 0; j < 8; j++)
-                crc = (ushort)((crc & 1) != 0 ? (crc >> 1) ^ poly : crc >> 1);
+            System.UInt16 crc = (System.UInt16)i;
+            for (System.Byte j = 0; j < 8; j++)
+                crc = (System.UInt16)((crc & 1) != 0 ? (crc >> 1) ^ poly : crc >> 1);
 
             table[i] = crc;
         }
@@ -47,14 +47,14 @@ internal static class Crc00
     /// Generates the CRC-32 lookup table based on the polynomial.
     /// </summary>
     /// <returns>A lookup table for CRC-32 computation.</returns>
-    internal static uint[] GenerateTable32(uint poly)
+    internal static System.UInt32[] GenerateTable32(System.UInt32 poly)
     {
-        var table = new uint[256];
+        System.UInt32[] table = new System.UInt32[256];
 
-        for (uint i = 0; i < 256; i++)
+        for (System.UInt32 i = 0; i < 256; i++)
         {
-            uint crc = i;
-            for (int j = 0; j < 8; j++)
+            System.UInt32 crc = i;
+            for (System.Byte j = 0; j < 8; j++)
                 crc = (crc & 1) != 0 ? (crc >> 1) ^ poly : crc >> 1;
 
             table[i] = crc;
@@ -68,13 +68,13 @@ internal static class Crc00
     /// </summary>
     /// <param name="poly">The polynomial to use for table generation.</param>
     /// <returns>A lookup table for CRC-64 computation.</returns>
-    internal static ulong[] GenerateTable64(ulong poly)
+    internal static System.UInt64[] GenerateTable64(System.UInt64 poly)
     {
-        var table = new ulong[256];
-        for (int i = 0; i < 256; i++)
+        System.UInt64[] table = new System.UInt64[256];
+        for (System.Int32 i = 0; i < 256; i++)
         {
-            ulong crc = (ulong)i;
-            for (int j = 0; j < 8; j++)
+            System.UInt64 crc = (System.UInt64)i;
+            for (System.Byte j = 0; j < 8; j++)
                 crc = (crc & 1) != 0 ? (crc >> 1) ^ poly : crc >> 1;
 
             table[i] = crc;
