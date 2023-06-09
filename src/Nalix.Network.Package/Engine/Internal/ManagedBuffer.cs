3bac4c4001b21f7f094005df2bc8f13a7ccd0b92
phucnguyen <nguyen098xx@gmail.com>
2023-06-09 11:40:18 +0700

    1.5.1

---
+++ src/Nalix.Network.Package/Engine/Internal/ManagedBuffer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Nalix.Network.Package.Engine.Internal;
+
+/// <summary>
+/// Managed buffer with ultra-optimized memory handling
+/// </summary>
+[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+internal readonly struct ManagedBuffer(ReadOnlyMemory<byte> memory, PoolHandle? handle) : IDisposable
+{
+    public readonly ReadOnlyMemory<byte> Memory = memory;
+    public readonly PoolHandle? Handle = handle;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Dispose()
+    {
+        Handle?.Dispose();
+    }
+}
+
+/// <summary>
+/// Pool handle using direct memory addresses for maximum performance
+/// </summary>
+[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+internal sealed class PoolHandle(nint address) : IDisposable
+{
+    public readonly nint Address = address;
+    private int _disposed;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Dispose()
+    {
+        if (Interlocked.Exchange(ref _disposed, 1) == 0)
+        {
+            MemoryVault.ReturnToPool(this);
+        }
+    }
+}
+
+/// <summary>
+/// Tracked memory entry using weak references for automatic cleanup
+/// </summary>
+[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+internal readonly struct TrackedMemory(byte[] array, long lastAccessTime, WeakReference<byte[]> weakRef)
+{
+    public readonly byte[] Array = array;
+    public readonly long LastAccessTime = lastAccessTime;
+    public readonly WeakReference<byte[]> WeakRef = weakRef;
+}
