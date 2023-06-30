8ee56a9f1855024b8dff65a657e1806e7049c8b0
phucnguyen <nguyen098xx@gmail.com>
2023-06-30 20:17:48 +0700

    Refactor packet handling and namespace organization
    - Updated suppression messages for primary constructors in `UdpTransport` and `PacketDispatchCore`.
    - Changed namespace from `Nalix.Network.Dispatch` to `Nalix.Network.Dispatch.Core` in multiple files.
    - Enhanced `PacketContext` with pooling support and zero-allocation design.
    - Introduced `PacketDescriptor` and `PacketHandlerDescriptor` for optimized packet handling.
    - Added new handler classes for processing return values with zero-allocation.
    - Implemented `ControllerScanner` for high-performance controller scanning.
    - Introduced `ReturnTypeHandlerFactory` for future management of return type handlers.

---
+++ src/Nalix.Network/Dispatch/Handlers/StringReturnHandler.cs
@@ -0,0 +1,22 @@
+﻿using Nalix.Network.Dispatch.Core;
+using System.Runtime.CompilerServices;
+
+namespace Nalix.Network.Dispatch.Handlers;
+
+/// <inheritdoc/>
+public sealed class StringReturnHandler<TPacket> : IReturnTypeHandler<TPacket>
+    where TPacket : Common.Package.IPacket, Common.Package.IPacketFactory<TPacket>
+{
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public async System.Threading.Tasks.ValueTask HandleAsync(
+        System.Object? result,
+        PacketContext<TPacket> context)
+    {
+        if (result is System.String data)
+        {
+            using var packet = TPacket.Create(0, data);
+            await context.Connection.Tcp.SendAsync(packet.Serialize());
+        }
+    }
+}
---
+++ src/Nalix.Network/Dispatch/Handlers/VoidReturnHandler.cs
@@ -0,0 +1,17 @@
+﻿using Nalix.Network.Dispatch.Core;
+using System.Runtime.CompilerServices;
+
+namespace Nalix.Network.Dispatch.Handlers;
+
+/// <inheritdoc/>
+public sealed class VoidReturnHandler<TPacket> : IReturnTypeHandler<TPacket>
+{
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public System.Threading.Tasks.ValueTask HandleAsync(
+        System.Object? result,
+        PacketContext<TPacket> context)
+    {
+        return System.Threading.Tasks.ValueTask.CompletedTask;
+    }
+}
