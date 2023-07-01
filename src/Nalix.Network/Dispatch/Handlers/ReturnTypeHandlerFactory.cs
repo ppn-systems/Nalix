3a5dab0c38368dd670fa7944a0a63bca5a4b5002
phucnguyen <nguyen098xx@gmail.com>
2023-07-01 15:16:58 +0700

    1.9.5
    Added caching and pooling capabilities to `PacketContext` and `ControllerScanner` classes to reduce allocations and improve method access efficiency. Implemented the `IPoolable` interface in `PacketContext` and introduced a `ResetForPool` method. Refactored `ControllerScanner` for better logging and error handling, and added new return type handlers like `ReadOnlyMemoryReturnHandler`, `TaskVoidReturnHandler`, and `ValueTaskVoidReturnHandler` for improved asynchronous result management. Updated `PacketDispatchOptions` to support logging and error handling configurations.

---
+++ src/Nalix.Network/Dispatch/Handlers/ReturnTypeHandlerFactory.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Frozen;
+using System.Runtime.CompilerServices;
+
+namespace Nalix.Network.Dispatch.Handlers;
+
+/// <summary>
+/// Factory để tạo return type handlers với zero allocation approach.
+/// </summary>
+/// <typeparam name="TPacket">Packet type</typeparam>
+public static class ReturnTypeHandlerFactory<TPacket>
+    where TPacket : Common.Package.IPacket,
+                   Common.Package.IPacketFactory<TPacket>,
+                   Common.Package.IPacketEncryptor<TPacket>,
+                   Common.Package.IPacketCompressor<TPacket>
+{
+    /// <summary>
+    /// Cached handlers để tránh recreation.
+    /// </summary>
+    private static readonly FrozenDictionary<System.Type, IReturnTypeHandler<TPacket>> _handlers = CreateHandlers();
+
+    /// <summary>
+    /// Get handler cho specific return type.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IReturnTypeHandler<TPacket> GetHandler(System.Type returnType)
+    {
+        if (_handlers.TryGetValue(returnType, out var handler))
+            return handler;
+
+        // Handle generic Task<T> và ValueTask<T>
+        if (returnType.IsGenericType)
+        {
+            var genericType = returnType.GetGenericTypeDefinition();
+            var genericArg = returnType.GetGenericArguments()[0];
+
+            if (genericType == typeof(System.Threading.Tasks.Task<>))
+            {
+                var innerHandler = GetHandler(genericArg);
+                return CreateTaskHandler(innerHandler, genericArg);
+            }
+
+            if (genericType == typeof(System.Threading.Tasks.ValueTask<>))
+            {
+                var innerHandler = GetHandler(genericArg);
+                return CreateValueTaskHandler(innerHandler, genericArg);
+            }
+        }
+
+        // Fallback cho unsupported types
+        return new UnsupportedReturnHandler<TPacket>(returnType);
+    }
+
+    /// <summary>
+    /// Create base handlers dictionary.
+    /// </summary>
+    private static FrozenDictionary<System.Type, IReturnTypeHandler<TPacket>> CreateHandlers()
+    {
+        var handlers = new System.Collections.Generic.Dictionary<System.Type, IReturnTypeHandler<TPacket>>
+        {
+            [typeof(void)] = new VoidReturnHandler<TPacket>(),
+            [typeof(TPacket)] = new PacketReturnHandler<TPacket>(),
+            [typeof(System.Byte[])] = new ByteArrayReturnHandler<TPacket>(),
+            [typeof(System.String)] = new StringReturnHandler<TPacket>(),
+            [typeof(System.Memory<System.Byte>)] = new MemoryReturnHandler<TPacket>(),
+            [typeof(System.ReadOnlyMemory<System.Byte>)] = new ReadOnlyMemoryReturnHandler<TPacket>(),
+            [typeof(System.Threading.Tasks.Task)] = new TaskVoidReturnHandler<TPacket>(),
+            [typeof(System.Threading.Tasks.ValueTask)] = new ValueTaskVoidReturnHandler<TPacket>(),
+        };
+
+        return handlers.ToFrozenDictionary();
+    }
+
+    private static IReturnTypeHandler<TPacket> CreateTaskHandler(
+        IReturnTypeHandler<TPacket> innerHandler,
+        System.Type resultType)
+    {
+        var handlerType = typeof(TaskReturnHandler<,>).MakeGenericType(typeof(TPacket), resultType);
+        return (IReturnTypeHandler<TPacket>)System.Activator.CreateInstance(handlerType, innerHandler)!;
+    }
+
+    private static IReturnTypeHandler<TPacket> CreateValueTaskHandler(
+        IReturnTypeHandler<TPacket> innerHandler,
+        System.Type resultType)
+    {
+        var handlerType = typeof(ValueTaskReturnHandler<,>).MakeGenericType(typeof(TPacket), resultType);
+        return (IReturnTypeHandler<TPacket>)System.Activator.CreateInstance(handlerType, innerHandler)!;
+    }
+}
---
+++ src/Nalix.Network/Dispatch/Options/PacketDispatchOptions.Fields.cs
@@ -0,0 +1,117 @@
+﻿using Nalix.Common.Logging;
+using Nalix.Common.Package;
+using Nalix.Network.Dispatch.Core;
+using Nalix.Network.Dispatch.Handlers;
+using Nalix.Network.Dispatch.Middleware;
+using Nalix.Network.Dispatch.Middleware.Inbound;
+
+namespace Nalix.Network.Dispatch.Options;
+
+/// <summary>
+/// Provides options for packet dispatching, including middleware configuration,
+/// error handling, and logging.
+/// </summary>
+/// <typeparam name="TPacket">The type of packet being dispatched.</typeparam>
+public sealed partial class PacketDispatchOptions<TPacket> where TPacket : IPacket,
+    IPacketFactory<TPacket>,
+    IPacketEncryptor<TPacket>,
+    IPacketCompressor<TPacket>
+{
+    #region Fields
+
+    private ILogger? _logger;
+
+    /// <summary>
+    /// Gets or sets a custom error-handling delegate invoked when packet processing fails.
+    /// </summary>
+    /// <remarks>
+    /// If not set, exceptions are only logged. You can override this to trigger alerts or retries.
+    /// </remarks>
+    private System.Action<System.Exception, System.UInt16>? _errorHandler;
+
+    private readonly System.Collections.Generic.Dictionary<
+        System.UInt16, PacketHandlerDescriptor<TPacket>> _handlerCache;
+
+    private readonly PacketMiddlewarePipeline<TPacket> _pipeline;
+
+    private PacketDispatchOptions()
+    {
+        _handlerCache = [];
+        _pipeline = new PacketMiddlewarePipeline<TPacket>();
+    }
+
+    #endregion Fields
+
+    #region Private Methods
+
+    /// <summary>
+    /// Configure default middleware pipeline.
+    /// </summary>
+    private void ConfigureDefaultMiddleware()
+    {
+        // Pre-processing middleware
+        _pipeline
+            .UsePre(new RateLimitMiddleware<TPacket>())
+            .UsePre(new DecompressionMiddleware<TPacket>())
+            .UsePre(new DecryptionMiddleware<TPacket>());
+        //.UsePre(new ValidationMiddleware<TPacket>());
+
+        // Post-processing middleware
+        //_pipeline
+        //    .UsePost(new CompressionMiddleware<TPacket>())
+        //    .UsePost(new EncryptionMiddleware<TPacket>())
+        //    .UsePost(new LoggingMiddleware<TPacket>(_logger));
+    }
+
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    private async System.Threading.Tasks.ValueTask ExecuteHandler(
+        PacketHandlerDescriptor<TPacket> descriptor,
+        PacketContext<TPacket> context)
+    {
+        // Validation check
+        //if (!descriptor.CanExecute(context))
+        //{
+        //}
+
+        try
+        {
+            // Execute compiled handler
+            System.Object? result = await descriptor.ExecuteAsync(context);
+
+            // Handle return value
+            IReturnTypeHandler<TPacket> returnHandler = ReturnTypeHandlerFactory<TPacket>.GetHandler(descriptor.ReturnType);
+            await returnHandler.HandleAsync(result, context);
+        }
+        catch (System.Exception ex)
+        {
+            await HandleExecutionException(descriptor, context, ex);
+        }
+    }
+
+    /// <summary>
+    /// Handle execution exception.
+    /// </summary>
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    private async System.Threading.Tasks.ValueTask HandleExecutionException(
+        PacketHandlerDescriptor<TPacket> descriptor,
+        PacketContext<TPacket> context,
+        System.Exception exception)
+    {
+        _logger?.Error("Handler execution failed for OpCode={0}: {1}",
+            descriptor.OpCode, exception.Message);
+
+        _errorHandler?.Invoke(exception, descriptor.OpCode);
+
+        TPacket errorPacket = TPacket.Create(0, "Internal server error");
+        await context.Connection.Tcp.SendAsync(errorPacket.Serialize());
+    }
+
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    private static T EnsureNotNull<T>(T value, string paramName) where T : class
+        => value ?? throw new System.ArgumentNullException(paramName);
+
+    #endregion Private Methods
+}
