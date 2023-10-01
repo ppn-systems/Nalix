d4df6a2bf609cfbac827e5e67abf0a1985a2d26e
phucnguyen <nguyen098xx@gmail.com>
2023-10-02 04:39:28 +0700

    3.1.0
    - Introduced `Nalix.Common.Logging` for improved logging.
    - Removed optional logger parameter from `Connection` and `TransportStream` constructors.
    - Updated `Send` and `BeginReceive` methods to use the new logging mechanism.
    - Refactored `PacketRegistry` to utilize `ConcurrentDictionary` for better thread safety.
    - Changed `HandlePacketAsync` to `HandlePacket` for synchronous handling.
    - Added `ExecuteHandler` method in `PacketDispatchOptions` for structured packet processing.
    - Improved error handling in `InstanceManager` with exception logging.
    - Updated `UnsupportedReturnHandler` to log warnings for unsupported return types.
    - Made minor adjustments for code clarity and maintainability.

--- src/Nalix.Network/Dispatch/ReturnTypes/UnsupportedReturnHandler.cs
+++ src/Nalix.Network/Dispatch/ReturnTypes/UnsupportedReturnHandler.cs
@@ -1,10 +1,14 @@
-﻿using Nalix.Network.Dispatch.Core;
+﻿using Nalix.Common.Logging;
+using Nalix.Network.Dispatch.Core;
+using Nalix.Shared.Injection;
 
 namespace Nalix.Network.Dispatch.ReturnTypes;
 
 /// <inheritdoc/>
 internal sealed class UnsupportedReturnHandler<TPacket>(System.Type returnType) : IReturnHandler<TPacket>
 {
+    private static readonly System.Collections.Concurrent.ConcurrentDictionary<System.Type, System.Boolean> _loggedTypes = new();
+
     /// <inheritdoc/>
     [System.Runtime.CompilerServices.MethodImpl(
         System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
@@ -12,7 +16,13 @@ internal sealed class UnsupportedReturnHandler<TPacket>(System.Type returnType) 
         System.Object? result,
         PacketContext<TPacket> context)
     {
-        // Log warning về unsupported type
+        if (_loggedTypes.TryAdd(returnType, true))
+        {
+            InstanceManager.Instance.GetExistingInstance<ILogger>()?.Warn(
+                $"[Dispatch] Unsupported return type '{returnType.Name}' encountered. " +
+                "Result will not be processed, but stored in context properties.");
+        }
+
         context.SetProperty("UnsupportedReturnType", returnType.Name);
         return System.Threading.Tasks.ValueTask.CompletedTask;
     }
