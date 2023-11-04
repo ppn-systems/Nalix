2eb3de8848c5e860436a1eb21d097a92abb4ee73
phucnguyen <nguyen098xx@gmail.com>
2023-11-04 10:04:28 +0700

    3.5.2
    Updated `MagicNumbers.cs` to include new packet types: `Binary128`, `Binary256`, `Binary512`, `Binary1024`, `Text256`, `Text512`, and `Text1024`.
    
    Modified middleware and execution handling to use `Text256` instead of `TextPacket`.
    
    Removed old `BinaryPacket` and `TextPacket` classes, introducing new classes for binary and text packets with appropriate serialization logic.
    
    Added new files for each packet type, enhancing the flexibility and clarity of the packet handling system.

---
+++ src/Nalix.Shared/Messaging/Text/Text512.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
+
+using Nalix.Common.Attributes;
+using Nalix.Common.Connection.Protocols;
+using Nalix.Common.Packets;
+using Nalix.Common.Packets.Enums;
+
+namespace Nalix.Shared.Messaging.Text;
+
+/// <inheritdoc/>
+[MagicNumber(MagicNumbers.Binary512)]
+public sealed class Text512 : Text256
+{
+    /// <inheritdoc/>
+    public new const System.Int32 DynamicSize = 512;
+
+    /// <summary>Initializes a new <see cref="Text512"/> with empty content.</summary>
+    public Text512()
+    {
+        Flags = PacketFlags.None;
+        Content = System.String.Empty;
+        Priority = PacketPriority.Normal;
+        Transport = TransportProtocol.Null;
+        OpCode = PacketConstants.OpCodeDefault;
+        MagicNumber = (System.UInt32)MagicNumbers.Text512;
+    }
+}
