984b8c1fcf3594a81ef72ac4d32e3a1df57eef82
phucnguyen <nguyen098xx@gmail.com>
2023-12-01 07:36:08 +0700

    3.8.6
    Updated `using` directives across multiple files to replace
    references to `Nalix.Common.Logging` and `Nalix.Common.Security.Identity`
    with their respective abstractions and models. This change promotes
    a more modular design, enhancing flexibility and maintainability.
    Additionally, consolidated security-related enumerations by
    replacing `Nalix.Common.Security.Cryptography.Enums` with
    `Nalix.Common.Security.Enums`.

--- src/Nalix.Shared/Messaging/Text/Text512.cs
+++ src/Nalix.Shared/Messaging/Text/Text512.cs
@@ -5,7 +5,7 @@ using Nalix.Common.Connection.Protocols;
 using Nalix.Common.Packets;
 using Nalix.Common.Packets.Abstractions;
 using Nalix.Common.Packets.Enums;
-using Nalix.Common.Security.Cryptography.Enums;
+using Nalix.Common.Security.Enums;
 using Nalix.Common.Serialization;
 using Nalix.Common.Serialization.Attributes;
 using Nalix.Shared.Extensions;
