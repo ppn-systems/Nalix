f4cc9ba05bb01b83389f2e6653a2bf17cfad3e36
phucnguyen <nguyen098xx@gmail.com>
2023-12-13 04:23:38 +0700

    4.0.1
    Introduced a new test project `Nalix.Network.Tests` targeting .NET 9.0, including essential testing packages. Implemented unit tests in `PacketCatalogTests`, `ConnectionLimiterTests`, `RequestLimiterTests`, and `TimeSynchronizerTests` to validate functionality in the `Nalix.Network` namespace. Tests cover packet deserialization, connection limiting, request validation, and time synchronization, ensuring code robustness.

---
+++ tests/Nalix.Network.Test/Throttling/ConnectionLimiterTests.cs
@@ -0,0 +1,55 @@
+﻿using Nalix.Network.Configurations;
+using Nalix.Network.Throttling;
+using System;
+using System.Net;
+using Xunit;
+
+namespace Nalix.Network.Tests.Throttling;
+
+/// <summary>
+/// Unit tests for <see cref="ConnectionLimiter"/>.
+/// </summary>
+public sealed class ConnectionLimiterTests : IDisposable
+{
+    private readonly ConnectionLimiter _limiter;
+
+    public ConnectionLimiterTests()
+    {
+        var options = new ConnLimitOptions
+        {
+            MaxConnectionsPerIpAddress = 2,
+            InactivityThresholdMs = 5000,
+            CleanupIntervalMs = 1000,
+        };
+        _limiter = new ConnectionLimiter(options);
+    }
+
+    [Fact]
+    public void IsConnectionAllowed_ShouldAllowUpToLimit_ThenBlock()
+    {
+        // Arrange
+        var ip = IPAddress.Loopback;
+
+        // Act + Assert
+        Assert.True(_limiter.IsConnectionAllowed(ip));
+        Assert.True(_limiter.IsConnectionAllowed(ip));
+        Assert.False(_limiter.IsConnectionAllowed(ip)); // third should be blocked
+    }
+
+    [Fact]
+    public void ConnectionClosed_ShouldDecrementCounters()
+    {
+        var ip = IPAddress.Loopback;
+
+        _ = _limiter.IsConnectionAllowed(ip);
+        _ = _limiter.IsConnectionAllowed(ip);
+        var (CurrentConnections, _, _) = _limiter.GetConnectionInfo(ip);
+        Assert.Equal(2, CurrentConnections);
+
+        Assert.True(_limiter.ConnectionClosed(ip));
+        var infoAfter = _limiter.GetConnectionInfo(ip);
+        Assert.Equal(1, infoAfter.CurrentConnections);
+    }
+
+    public void Dispose() => _limiter.Dispose();
+}
---
+++ tests/Nalix.Network.Test/Throttling/RequestLimiterTests.cs
@@ -0,0 +1,40 @@
+﻿using Nalix.Common.Exceptions;
+using Nalix.Network.Configurations;
+using Nalix.Network.Throttling;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nalix.Network.Tests.Throttling;
+
+/// <summary>
+/// Unit tests for <see cref="RequestLimiter"/> config & input validation.
+/// </summary>
+public sealed class RequestLimiterTests
+{
+    [Fact]
+    public void Ctor_ShouldThrow_ForInvalidConfiguration()
+    {
+        var invalid = new RateLimitOptions
+        {
+            MaxAllowedRequests = 0, // invalid
+            TimeWindowInMilliseconds = 1000,
+            LockoutDurationSeconds = 1
+        };
+
+        _ = Assert.Throws<InternalErrorException>(() => new RequestLimiter(invalid));
+    }
+
+    [Fact]
+    public async Task CheckLimitAsync_ShouldThrow_ForNullOrWhitespaceEndpoint()
+    {
+        var valid = new RateLimitOptions
+        {
+            MaxAllowedRequests = 10,
+            TimeWindowInMilliseconds = 1000,
+            LockoutDurationSeconds = 1
+        };
+
+        var limiter = new RequestLimiter(valid);
+        _ = await Assert.ThrowsAsync<InternalErrorException>(() => limiter.CheckLimitAsync("   ").AsTask());
+    }
+}
