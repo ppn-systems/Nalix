69d013d72b6257dcf582588c61f0273e14376b94
phucnguyen <nguyen098xx@gmail.com>
2024-01-20 03:43:38 +0700

    4.4.9
    Updated version numbers across multiple project files from `4.2.3` to `5.0.0`.
    
    In `Connection.cs`, added a new `ConnectionEventArgs` instance for better event handling and modified packet processing.
    
    Refactored `ProtocolChannel` to implement a new caching mechanism for incoming packets and introduced `ProtocolSessionCache` for improved packet management.
    
    Enhanced resource management in `Dispose` methods and removed outdated packet handling methods, improving performance and maintainability of the networking components.

--- src/Nalix.Network/Internal/Protocols/ProtocolSessionCache.cs
+++ src/Nalix.Network/Internal/Protocols/ProtocolSessionCache.cs
@@ -1,10 +1,10 @@
-// Copyright (c) 2025 PPN Corporation. All rights reserved.
+﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
 
+using Nalix.Common.Connection;
 using Nalix.Framework.Time;
 using Nalix.Shared.Configuration;
 using Nalix.Shared.Memory.Buffers;
 using Nalix.Shared.Memory.Caches;
-using Nalix.Shared.Memory.Pooling;
 
 #if DEBUG
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Nalix.Network.Tests")]
@@ -19,6 +19,10 @@ namespace Nalix.Network.Internal.Protocols;
 internal sealed class ProtocolSessionCache : System.IDisposable
 {
     #region Fields
+
+    private IConnection? _sender;
+    private IConnectEventArgs? _cachedArgs;
+    private System.EventHandler<IConnectEventArgs>? _callback;
 
     private static Configurations.CacheSizeOptions Config
         => ConfigurationManager.Instance.Get<Configurations.CacheSizeOptions>();
@@ -40,11 +44,6 @@ internal sealed class ProtocolSessionCache : System.IDisposable
     public System.Int64 LastPingTime { get; set; }
 
     /// <summary>
-    /// Occurs when a new incoming packet is added to the cache.
-    /// </summary>
-    public event System.Action? PacketCached;
-
-    /// <summary>
     /// Gets the cache that stores recently received (incoming) packets.
     /// </summary>
     public readonly FifoCache<BufferLease> Incoming;
@@ -60,7 +59,20 @@ internal sealed class ProtocolSessionCache : System.IDisposable
     #region Public Methods
 
     /// <summary>
-    /// Adds a received packet to the incoming cache and triggers the <see cref="PacketCached"/> event.
+    /// Registers a callback to be invoked when a packet is cached. The state is passed back as the argument.
+    /// </summary>
+    public void SetCallback(
+        System.EventHandler<IConnectEventArgs>? callback,
+        IConnection sender,
+        IConnectEventArgs args)
+    {
+        _callback = callback;
+        _sender = sender ?? throw new System.ArgumentNullException(nameof(sender));
+        _cachedArgs = args ?? throw new System.ArgumentNullException(nameof(args));
+    }
+
+    /// <summary>
+    /// Adds a received packet to the incoming cache and triggers the cache event.
     /// </summary>
     /// <param name="data">The received packet data.</param>
     [System.Runtime.CompilerServices.MethodImpl(
@@ -68,7 +80,9 @@ internal sealed class ProtocolSessionCache : System.IDisposable
     public void PushIncoming(BufferLease data)
     {
         this.Incoming.Push(data);
-        PacketCached?.Invoke();
+
+        // No-alloc hot path: dùng sẵn sender + cached args
+        _callback?.Invoke(_sender!, _cachedArgs!);
     }
 
     /// <summary>
@@ -82,64 +96,4 @@ internal sealed class ProtocolSessionCache : System.IDisposable
     }
 
     #endregion Public Methods
-
-    #region Private Methods
-
-    internal static unsafe System.UInt64 HashToUInt64(System.ReadOnlySpan<System.Byte> data)
-    {
-        const System.Int32 r = 47;
-        const System.UInt64 s = 0xc70f6907UL;
-        const System.UInt64 m = 0xc6a4a7935bd1e995UL;
-
-        System.UInt64 h = s ^ ((System.UInt64)data.Length * m);
-
-        fixed (System.Byte* pBase = data)
-        {
-            System.Byte* p = pBase;
-            System.Int32 len = data.Length;
-
-            // body
-            while (len >= 8)
-            {
-                // unaligned read
-                System.UInt64 k = *(System.UInt64*)p;
-                if (!System.BitConverter.IsLittleEndian)
-                {
-                    k = System.Buffers.Binary.BinaryPrimitives.ReverseEndianness(k);
-                }
-
-                k *= m;
-                k ^= k >> r;
-                k *= m;
-
-                h ^= k;
-                h *= m;
-
-                p += 8;
-                len -= 8;
-            }
-
-            // tail (0..7 bytes), pack little-endian without alloc
-            if (len > 0)
-            {
-                System.UInt64 k = 0;
-                for (System.Int32 i = 0; i < len; i++)
-                {
-                    k |= (System.UInt64)p[i] << (8 * i);
-                }
-
-                h ^= k;
-                h *= m;
-            }
-        }
-
-        // finalization
-        h ^= h >> r;
-        h *= m;
-        h ^= h >> r;
-
-        return h;
-    }
-
-    #endregion Private Methods
 }
