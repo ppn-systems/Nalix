4164a54b978351337cecf068be0ad031ece89ced
phucnguyen <nguyen098xx@gmail.com>
2024-09-15 04:00:00 +0700

    1.3.2

--- src/Nalix/Randomization/GRandom.cs
+++ src/Nalix/Randomization/GRandom.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-
 namespace Nalix.Randomization;
 
 /// <summary>
@@ -18,12 +14,12 @@ public sealed class GRandom(System.Int32 seed)
     /// <summary>
     /// The maximum possible generated integer value.
     /// </summary>
-    public const int RandMax = 0x7FFFFFFF;
+    public const System.Int32 RandMax = 0x7FFFFFFF;
 
     /// <summary>
     /// Inverse of RandMax as a double for faster calculations.
     /// </summary>
-    public const double InvRandMax = 1.0 / RandMax;
+    public const System.Double InvRandMax = 1.0 / RandMax;
 
     #endregion Constants
 
@@ -32,7 +28,7 @@ public sealed class GRandom(System.Int32 seed)
     /// <summary>
     /// Current seed value for the random Number generator.
     /// </summary>
-    private int _seed = seed;
+    private System.Int32 _seed = seed;
 
     /// <summary>
     /// Random Number generator instance.
@@ -58,52 +54,56 @@ public sealed class GRandom(System.Int32 seed)
     /// Resets the seed for the random Number generator.
     /// </summary>
     /// <param name="seed">The new seed value.</param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Seed(int seed)
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public void Seed(System.Int32 seed)
     {
         _seed = seed;
-        _rand.SetSeed((uint)seed);
+        _rand.SetSeed((System.UInt32)seed);
     }
 
     /// <summary>
     /// Gets the current seed value.
     /// </summary>
     /// <returns>The current seed value.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public int GetSeed() => _seed;
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Int32 GetSeed() => _seed;
 
     /// <summary>
     /// Generates a random integer in the range [0, RandMax].
     /// </summary>
     /// <returns>A random integer.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public int Next() => (int)(_rand.Get() & RandMax);
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Int32 Next() => (System.Int32)(_rand.Get() & RandMax);
 
     /// <summary>
     /// Generates a random integer in the range [0, max).
     /// </summary>
     /// <param name="max">The exclusive upper bound.</param>
     /// <returns>A random integer.</returns>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown when max is less than or equal to 0.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public int Next(int max)
+    /// <exception cref="System.ArgumentOutOfRangeException">Thrown when max is less than or equal to 0.</exception>
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Int32 Next(System.Int32 max)
     {
         if (max <= 0)
-            throw new ArgumentOutOfRangeException(nameof(max), "Max must be positive");
+            throw new System.ArgumentOutOfRangeException(nameof(max), "Max must be positive");
 
         // Fast path for power of 2
         if ((max & (max - 1)) == 0)
-            return (int)(((long)(_rand.Get() & RandMax) * max) >> 31);
+            return (System.Int32)(((_rand.Get() & RandMax) * max) >> 31);
 
         // Avoid modulo bias by rejecting values in the unfair region
-        uint threshold = (uint)((RandMax - (RandMax % max)) & RandMax);
-        uint result;
+        System.UInt32 threshold = (System.UInt32)((RandMax - (RandMax % max)) & RandMax);
+        System.UInt32 result;
         do
         {
             result = _rand.Get() & RandMax;
         } while (result >= threshold);
 
-        return (int)(result % max);
+        return (System.Int32)(result % max);
     }
 
     /// <summary>
@@ -112,39 +112,42 @@ public sealed class GRandom(System.Int32 seed)
     /// <param name="min">The inclusive lower bound.</param>
     /// <param name="max">The exclusive upper bound.</param>
     /// <returns>A random integer.</returns>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown when min is greater than or equal to max.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public int Next(int min, int max)
+    /// <exception cref="System.ArgumentOutOfRangeException">Thrown when min is greater than or equal to max.</exception>
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Int32 Next(System.Int32 min, System.Int32 max)
     {
         if (min >= max)
         {
             if (min == max)
                 return min;
-            throw new ArgumentOutOfRangeException(nameof(min), "Min must be less than max");
+            throw new System.ArgumentOutOfRangeException(nameof(min), "Min must be less than max");
         }
 
-        long range = (long)max - min;
-        if (range <= int.MaxValue)
-            return min + Next((int)range);
+        System.Int64 range = (System.Int64)max - min;
+        if (range <= System.Int32.MaxValue)
+            return min + Next((System.Int32)range);
 
         // Handle large ranges that exceed int.MaxValue
-        return min + (int)(NextDouble() * range);
+        return min + (System.Int32)(NextDouble() * range);
     }
 
     /// <summary>
     /// Generates a random floating-point Number in the range [0.0f, 1.0f].
     /// </summary>
     /// <returns>A random float.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public float NextFloat() => _rand.GetFloat();
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Single NextFloat() => _rand.GetFloat();
 
     /// <summary>
     /// Generates a random floating-point Number in the range [0.0f, max).
     /// </summary>
     /// <param name="max">The exclusive upper bound.</param>
     /// <returns>A random float.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public float NextFloat(float max) => NextFloat() * max;
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Single NextFloat(System.Single max) => NextFloat() * max;
 
     /// <summary>
     /// Generates a random floating-point Number in the range [min, max).
@@ -152,23 +155,26 @@ public sealed class GRandom(System.Int32 seed)
     /// <param name="min">The inclusive lower bound.</param>
     /// <param name="max">The exclusive upper bound.</param>
     /// <returns>A random float.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public float NextFloat(float min, float max) => min + (NextFloat() * (max - min));
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Single NextFloat(System.Single min, System.Single max) => min + (NextFloat() * (max - min));
 
     /// <summary>
     /// Generates a random double-precision floating-point Number in the range [0.0, 1.0].
     /// </summary>
     /// <returns>A random double.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public double NextDouble() => _rand.GetDouble();
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Double NextDouble() => _rand.GetDouble();
 
     /// <summary>
     /// Generates a random double-precision floating-point Number in the range [0.0, max).
     /// </summary>
     /// <param name="max">The exclusive upper bound.</param>
     /// <returns>A random double.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public double NextDouble(double max) => NextDouble() * max;
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Double NextDouble(System.Double max) => NextDouble() * max;
 
     /// <summary>
     /// Generates a random double-precision floating-point Number in the range [min, max).
@@ -176,16 +182,18 @@ public sealed class GRandom(System.Int32 seed)
     /// <param name="min">The inclusive lower bound.</param>
     /// <param name="max">The exclusive upper bound.</param>
     /// <returns>A random double.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public double NextDouble(double min, double max) => min + (NextDouble() * (max - min));
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Double NextDouble(System.Double min, System.Double max) => min + (NextDouble() * (max - min));
 
     /// <summary>
     /// Performs a random check with a given percentage probability.
     /// </summary>
     /// <param name="pct">The percentage probability (0-100).</param>
     /// <returns>True if the random check passed based on the specified percentage.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool NextPct(int pct)
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Boolean NextPct(System.Int32 pct)
     {
         if (pct <= 0) return false;
         if (pct >= 100) return true;
@@ -197,8 +205,9 @@ public sealed class GRandom(System.Int32 seed)
     /// </summary>
     /// <param name="probability">The probability (0.0-1.0).</param>
     /// <returns>True if the random check passed based on the specified probability.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool NextProbability(double probability)
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public System.Boolean NextProbability(System.Double probability)
     {
         if (probability <= 0.0) return false;
         if (probability >= 1.0) return true;
@@ -210,15 +219,15 @@ public sealed class GRandom(System.Int32 seed)
     /// </summary>
     /// <typeparam name="T">The type of elements in the list.</typeparam>
     /// <param name="list">The list to shuffle.</param>
-    public void ShuffleList<T>(IList<T> list)
+    public void ShuffleList<T>(System.Collections.Generic.IList<T> list)
     {
-        ArgumentNullException.ThrowIfNull(list);
+        System.ArgumentNullException.ThrowIfNull(list);
 
-        int n = list.Count;
+        System.Int32 n = list.Count;
         while (n > 1)
         {
             n--;
-            int k = Next(n + 1);
+            System.Int32 k = Next(n + 1);
             (list[k], list[n]) = (list[n], list[k]);
         }
     }
@@ -228,13 +237,13 @@ public sealed class GRandom(System.Int32 seed)
     /// </summary>
     /// <typeparam name="T">The type of elements in the span.</typeparam>
     /// <param name="span">The span to shuffle.</param>
-    public void ShuffleSpan<T>(Span<T> span)
+    public void ShuffleSpan<T>(System.Span<T> span)
     {
-        int n = span.Length;
+        System.Int32 n = span.Length;
         while (n > 1)
         {
             n--;
-            int k = Next(n + 1);
+            System.Int32 k = Next(n + 1);
             (span[k], span[n]) = (span[n], span[k]);
         }
     }
@@ -245,11 +254,11 @@ public sealed class GRandom(System.Int32 seed)
     /// <typeparam name="T">The type of elements in the list.</typeparam>
     /// <param name="list">The list to pick from.</param>
     /// <returns>A random item from the list.</returns>
-    /// <exception cref="ArgumentException">Thrown when the list is empty.</exception>
-    public T Choose<T>(IList<T> list)
+    /// <exception cref="System.ArgumentException">Thrown when the list is empty.</exception>
+    public T Choose<T>(System.Collections.Generic.IList<T> list)
     {
         if (list == null || list.Count == 0)
-            throw new ArgumentException("Cannot choose from an empty list", nameof(list));
+            throw new System.ArgumentException("Cannot choose from an empty list", nameof(list));
 
         return list[Next(list.Count)];
     }
@@ -260,11 +269,11 @@ public sealed class GRandom(System.Int32 seed)
     /// <typeparam name="T">The type of elements in the span.</typeparam>
     /// <param name="span">The span to pick from.</param>
     /// <returns>A random item from the span.</returns>
-    /// <exception cref="ArgumentException">Thrown when the span is empty.</exception>
-    public T Choose<T>(ReadOnlySpan<T> span)
+    /// <exception cref="System.ArgumentException">Thrown when the span is empty.</exception>
+    public T Choose<T>(System.ReadOnlySpan<T> span)
     {
         if (span.IsEmpty)
-            throw new ArgumentException("Cannot choose from an empty span", nameof(span));
+            throw new System.ArgumentException("Cannot choose from an empty span", nameof(span));
 
         return span[Next(span.Length)];
     }
@@ -273,18 +282,19 @@ public sealed class GRandom(System.Int32 seed)
     /// Fills the specified buffer with random bytes.
     /// </summary>
     /// <param name="buffer">The buffer to fill.</param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void NextBytes(Span<byte> buffer)
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public void NextBytes(System.Span<System.Byte> buffer)
     {
         for (int i = 0; i < buffer.Length; i++)
-            buffer[i] = (byte)Next(256);
+            buffer[i] = (System.Byte)Next(256);
     }
 
     /// <summary>
     /// Returns a string representation of the random Number generator state.
     /// </summary>
     /// <returns>A string representation of the RNG.</returns>
-    public override string ToString() => $"GRandom(seed={_seed}): {_rand}";
+    public override System.String ToString() => $"GRandom(seed={_seed}): {_rand}";
 
     #endregion Public Methods
 }
