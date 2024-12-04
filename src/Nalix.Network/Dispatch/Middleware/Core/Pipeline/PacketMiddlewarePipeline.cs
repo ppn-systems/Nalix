d4e89e45d40b6eee0e591c08b558f58801682432
phucnguyen <nguyen098xx@gmail.com>
2024-12-04 14:00:00 +0700

    3.3.9
    Significantly restructured the codebase to enhance organization and performance of the packet dispatching system. Key updates include the introduction of a new namespace for context-related classes, enhancements to the `IPacketDispatch` interface for flexible packet handling, and improvements to the `PacketContext` class for object pooling and zero-allocation design. Refined dispatcher classes for better logging and error handling, optimized `PacketHandlerDelegate` for performance, and introduced `PacketMetadata` for comprehensive packet attributes. Added extensive documentation and ensured support for advanced dependency injection and asynchronous processing.

--- src/Nalix.Network/Dispatch/Middleware/Core/Pipeline/PacketMiddlewarePipeline.cs
+++ src/Nalix.Network/Dispatch/Middleware/Core/Pipeline/PacketMiddlewarePipeline.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
 
-using Nalix.Network.Dispatch.Core;
+using Nalix.Network.Dispatch.Core.Context;
 using Nalix.Network.Dispatch.Middleware.Core.Interfaces;
 
 namespace Nalix.Network.Dispatch.Middleware.Core.Pipeline;
