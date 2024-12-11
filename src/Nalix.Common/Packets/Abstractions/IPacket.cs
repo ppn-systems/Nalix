f2947ec6e9825d22454357cdb27a2c91f364745c
phucnguyen <nguyen098xx@gmail.com>
2024-12-11 20:59:28 +0700

    8.6.0
    Updated method signatures to simplify parameters and improve nullability annotations:
    - Removed `[NotNull]` attributes from `ToString` in `IIdentifier.cs` and `Serialize` in `IPacket.cs`.
    - Added `[NotNull]` and `[NotNullWhen(true)]` annotations to `TryFormatBase36` and `TryWriteBytes` in `Identifier.Formatting.cs`.
    - Renamed `useHexFormat` to `isHex` in `ToString` in `Identifier.String.cs` for consistency.

--- src/Nalix.Common/Packets/Abstractions/IPacket.cs
+++ src/Nalix.Common/Packets/Abstractions/IPacket.cs
@@ -78,7 +78,7 @@ public interface IPacket
     [System.Diagnostics.Contracts.Pure]
     [System.Runtime.CompilerServices.MethodImpl(
         System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-    System.Int32 Serialize([System.Diagnostics.CodeAnalysis.NotNull] System.Span<System.Byte> buffer);
+    System.Int32 Serialize(System.Span<System.Byte> buffer);
 
     #endregion Packet Methods
 }
