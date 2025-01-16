405ea456897cd4335f56a41c6bc61b3d7565dcf0
PhcNguyen <phcnguyenz@proton.me>
2025-01-16 18:18:03 +0700

    0.0.78

---
+++ Source/Notio.Http/Exceptions/HttpTimeoutException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Notio.Http.Exceptions;
+
+/// <summary>
+/// An exception that is thrown when an HTTP call made by Flurl.Http times out.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="HttpTimeoutException"/> class.
+/// </remarks>
+/// <param name="call">Details of the HTTP call that caused the exception.</param>
+/// <param name="inner">The inner exception.</param>
+public class HttpTimeoutException(NotioCall call, Exception inner) : HttpException(call, BuildMessage(call), inner)
+{
+    private static string BuildMessage(NotioCall call) =>
+        call == null ? "Call timed out." : $"Call timed out: {call}";
+}
---
+++ Source/Notio.Http/Exceptions/ParsingException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Notio.Http.Exceptions;
+
+/// <summary>
+/// An exception that is thrown when an HTTP response could not be parsed to a particular format.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="ParsingException"/> class.
+/// </remarks>
+/// <param name="call">Details of the HTTP call that caused the exception.</param>
+/// <param name="expectedFormat">The format that could not be parsed to, i.e. JSON.</param>
+/// <param name="inner">The inner exception.</param>
+public class ParsingException(NotioCall call, string expectedFormat, Exception inner)
+    : HttpException(call, BuildMessage(call, expectedFormat), inner)
+{
+    /// <summary>
+    /// The format that could not be parsed to, i.e. JSON.
+    /// </summary>
+    public string ExpectedFormat { get; } = expectedFormat;
+
+    private static string BuildMessage(NotioCall call, string expectedFormat)
+    {
+        var msg = $"Response could not be deserialized to {expectedFormat}";
+        return msg + (call == null ? "." : $": {call}");
+    }
+}
