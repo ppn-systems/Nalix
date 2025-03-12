54eefba8d86c4807e5af07c9067111ce1c712e19
PhucNguyen <nguyen098xx@gmail.com>
2025-03-12 12:52:10 +0700

    0.3.60

--- src/Notio.Common/Attributes/JsonIncludeAttribute.cs
+++ src/Notio.Common/Attributes/JsonIncludeAttribute.cs
@@ -5,6 +5,10 @@ namespace Notio.Common.Attributes;
 /// <summary>
 /// Attribute to indicate that a property should be included in JSON serialization.
 /// </summary>
+/// <seealso cref="Attribute" />
+/// <remarks>
+/// Initializes a new instance of the <see cref="JsonIncludeAttribute" /> class.
+/// </remarks>
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
 public class JsonIncludeAttribute : Attribute
 {
