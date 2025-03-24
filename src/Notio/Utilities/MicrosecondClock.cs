f7f6d1eacb3cdbb26dacd9460f732006ada613a8
PhucNguyen <nguyen098xx@gmail.com>
2025-03-24 14:41:03 +0700

    0.4.11

--- src/Notio/Utilities/MicrosecondClock.cs
+++ src/Notio/Utilities/MicrosecondClock.cs
@@ -3,10 +3,22 @@ using System.Runtime.CompilerServices;
 
 namespace Notio.Utilities;
 
+/// <summary>
+/// Provides a high-resolution timestamp in microseconds.
+/// </summary>
 public static class MicrosecondClock
 {
+    /// <summary>
+    /// Conversion factor from stopwatch ticks to microseconds.
+    /// </summary>
+    public static readonly double TickMicroseconds =
+        (ConstantsDefault.MicrosecondsPerSecond / Stopwatch.Frequency);
+
+    /// <summary>
+    /// Gets the current timestamp in microseconds.
+    /// </summary>
+    /// <returns>The number of microseconds elapsed since an arbitrary point in time.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ulong GetTimestamp()
-        => (ulong)(Stopwatch.GetTimestamp() /
-           (Stopwatch.Frequency / ConstantsDefault.MicrosecondsPerSecond));
+        => (ulong)(Stopwatch.GetTimestamp() * TickMicroseconds);
 }
