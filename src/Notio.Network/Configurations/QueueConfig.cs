62fe619c2501ef472751eb29d5938020c7c9db9c
PhucNguyen <nguyen098xx@gmail.com>
2025-04-19 21:27:36 +0700

    0.6.33

--- src/Notio.Network/Configurations/QueueConfig.cs
+++ src/Notio.Network/Configurations/QueueConfig.cs
@@ -3,34 +3,48 @@ namespace Notio.Network.Configurations;
 /// <summary>
 /// Configuration options for PacketPriorityQueue
 /// </summary>
-/// <remarks>
-/// Initializes a new instance of <see cref="QueueConfig"/>.
-/// </remarks>
-/// <param name="maxCapacity">Maximum number of packets in the queue (0 = unlimited).</param>
-/// <param name="enableValidation">Indicates whether packet validity should be checked when dequeuing.</param>
-/// <param name="enableMetrics">Indicates whether detailed statistics should be collected.</param>
-/// <param name="timeout">Maximum time a packet is allowed to exist in the queue.</param>
-public sealed class QueueConfig(int maxCapacity = 0, System.TimeSpan? timeout = null,
-    bool enableValidation = true, bool enableMetrics = false) : Shared.Configuration.ConfigurationBinder
+public sealed class QueueConfig : Shared.Configuration.ConfigurationBinder
 {
     /// <summary>
     /// Maximum number of packets in the queue (0 = unlimited)
     /// </summary>
-    public int MaxCapacity { get; set; } = maxCapacity;
+    public int MaxCapacity { get; set; }
 
     /// <summary>
     /// Check packet validity when dequeuing
     /// </summary>
-    public bool EnableValidation { get; set; } = enableValidation;
+    public bool EnableValidation { get; set; }
 
     /// <summary>
     /// Collect detailed statistics
     /// </summary>
-    public bool EnableMetrics { get; set; } = enableMetrics;
+    public bool EnableMetrics { get; set; }
+
+    /// <summary>
+    /// Maximum time a packet is allowed to exist in the queue
+    /// </summary>
+    internal int TimeoutSeconds { get; set; } = 30;
 
     /// <summary>
     /// Maximum time a packet is allowed to exist in the queue
     /// </summary>
     [Shared.Configuration.Attributes.ConfiguredIgnore]
-    public System.TimeSpan Timeout { get; set; } = timeout ?? System.TimeSpan.FromSeconds(60);
+    public System.TimeSpan Timeout { get; set; }
+
+    /// <remarks>
+    /// Initializes a new instance of <see cref="QueueConfig"/>.
+    /// </remarks>
+    /// <param name="maxCapacity">Maximum number of packets in the queue (0 = unlimited).</param>
+    /// <param name="enableValidation">Indicates whether packet validity should be checked when dequeuing.</param>
+    /// <param name="enableMetrics">Indicates whether detailed statistics should be collected.</param>
+    /// <param name="timeout">Maximum time a packet is allowed to exist in the queue.</param>
+    public QueueConfig(
+        int maxCapacity = 0, System.TimeSpan? timeout = null,
+        bool enableValidation = true, bool enableMetrics = false)
+    {
+        this.MaxCapacity = maxCapacity;
+        this.EnableMetrics = enableMetrics;
+        this.EnableValidation = enableValidation;
+        this.Timeout = timeout ?? System.TimeSpan.FromSeconds(this.TimeoutSeconds);
+    }
 }
