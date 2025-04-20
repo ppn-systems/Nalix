2e231a2c6e3b31e88c4036332e26a7609f23e15d
PhucNguyen <nguyen098xx@gmail.com>
2025-04-20 22:26:01 +0700

    0.6.47

---
+++ src/Notio.Logging/CLogging.Method.cs
@@ -0,0 +1,149 @@
+using Notio.Common.Logging;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Notio.Logging;
+
+public sealed partial class CLogging
+{
+    #region Meta Methods
+
+    /// <inheritdoc />
+    public void Meta(string message)
+        => WriteLog(LogLevel.Meta, EventId.Empty, message);
+
+    /// <inheritdoc />
+    public void Meta(string format, params object[] args)
+        => base.CreateFormattedLogEntry(LogLevel.Meta, EventId.Empty, format, args);
+
+    /// <inheritdoc />
+    public void Meta(string message, EventId? eventId = null)
+        => WriteLog(LogLevel.Meta, eventId ?? EventId.Empty, message);
+
+    #endregion
+
+    #region Trace Methods
+
+    /// <inheritdoc />
+    public void Trace(string message)
+        => WriteLog(LogLevel.Trace, EventId.Empty, SanitizeLogMessage(message));
+
+    /// <inheritdoc />
+    public void Trace(string format, params object[] args)
+        => base.CreateFormattedLogEntry(LogLevel.Trace, EventId.Empty, format, args);
+
+    /// <inheritdoc />
+    public void Trace(string message, EventId? eventId = null)
+        => WriteLog(LogLevel.Trace, eventId ?? EventId.Empty, SanitizeLogMessage(message));
+
+    #endregion
+
+    #region Debug Methods
+
+    /// <inheritdoc />
+    public void Debug(string message)
+        => WriteLog(LogLevel.Debug, EventId.Empty, SanitizeLogMessage(message));
+
+    /// <inheritdoc />
+    public void Debug(string format, params object[] args)
+        => base.CreateFormattedLogEntry(LogLevel.Debug, EventId.Empty, format, args);
+
+    /// <inheritdoc />
+    public void Debug(string message, EventId? eventId = null)
+        => WriteLog(LogLevel.Debug, eventId ?? EventId.Empty, SanitizeLogMessage(message));
+
+    /// <inheritdoc />
+    public void Debug<TClass>(string message, EventId? eventId = null, [CallerMemberName] string memberName = "")
+        where TClass : class
+        => WriteLog(LogLevel.Debug, eventId ?? EventId.Empty, $"[{typeof(TClass).Name}:{memberName}] {message}");
+
+    #endregion
+
+    #region Info Methods
+
+    /// <inheritdoc />
+    public void Info(string message)
+        => WriteLog(LogLevel.Information, EventId.Empty, message);
+
+    /// <inheritdoc />
+    public void Info(string format, params object[] args)
+        => base.CreateFormattedLogEntry(LogLevel.Information, EventId.Empty, format, args);
+
+    /// <inheritdoc />
+    public void Info(string message, EventId? eventId = null)
+        => WriteLog(LogLevel.Information, eventId ?? EventId.Empty, message);
+
+    #endregion
+
+    #region Warn Methods
+
+    /// <inheritdoc />
+    public void Warn(string message)
+        => WriteLog(LogLevel.Warning, EventId.Empty, message);
+
+    /// <inheritdoc />
+    public void Warn(string format, params object[] args)
+        => base.CreateFormattedLogEntry(LogLevel.Warning, EventId.Empty, format, args);
+
+    /// <inheritdoc />
+    public void Warn(string message, EventId? eventId = null)
+        => WriteLog(LogLevel.Warning, eventId ?? EventId.Empty, message);
+
+    #endregion
+
+    #region Error Methods
+
+    /// <inheritdoc />
+    public void Error(string message)
+        => WriteLog(LogLevel.Error, EventId.Empty, message);
+
+    /// <inheritdoc />
+    public void Error(string format, params object[] args)
+        => base.CreateFormattedLogEntry(LogLevel.Error, EventId.Empty, format, args);
+
+    /// <inheritdoc />
+    public void Error(Exception exception)
+        => WriteLog(LogLevel.Error, EventId.Empty, exception.Message, exception);
+
+    /// <inheritdoc />
+    public void Error(string message, EventId? eventId = null)
+        => WriteLog(LogLevel.Error, eventId ?? EventId.Empty, message);
+
+    /// <inheritdoc />
+    public void Error(Exception exception, EventId? eventId = null)
+        => WriteLog(LogLevel.Error, eventId ?? EventId.Empty, exception.Message, exception);
+
+    /// <inheritdoc />
+    public void Error(string message, Exception exception)
+        => WriteLog(LogLevel.Error, EventId.Empty, message, exception);
+
+    /// <inheritdoc />
+    public void Error(string message, Exception exception, EventId? eventId = null)
+        => WriteLog(LogLevel.Error, eventId ?? EventId.Empty, message, exception);
+
+    #endregion
+
+    #region Fatal Methods
+
+    /// <inheritdoc />
+    public void Fatal(string message)
+        => WriteLog(LogLevel.Critical, EventId.Empty, message);
+
+    /// <inheritdoc />
+    public void Fatal(string format, params object[] args)
+        => base.CreateFormattedLogEntry(LogLevel.Critical, EventId.Empty, format, args);
+
+    /// <inheritdoc />
+    public void Fatal(string message, EventId? eventId = null)
+        => WriteLog(LogLevel.Critical, eventId ?? EventId.Empty, message);
+
+    /// <inheritdoc />
+    public void Fatal(string message, Exception exception)
+        => WriteLog(LogLevel.Critical, EventId.Empty, message, exception);
+
+    /// <inheritdoc />
+    public void Fatal(string message, Exception exception, EventId? eventId = null)
+        => WriteLog(LogLevel.Critical, eventId ?? EventId.Empty, message, exception);
+
+    #endregion
+}
