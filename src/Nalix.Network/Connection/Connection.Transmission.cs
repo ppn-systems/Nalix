f354ae08a7f9b109933a9604041fa362d28d2ef1
phucnguyen <nguyen098xx@gmail.com>
2025-04-28 15:24:18 +0700

    0.7.37

--- src/Nalix.Network/Connection/Connection.Transmission.cs
+++ src/Nalix.Network/Connection/Connection.Transmission.cs
@@ -1,26 +1,26 @@
 using Nalix.Common.Connection;
 using Nalix.Common.Package;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Nalix.Network.Connection;
 
 public sealed partial class Connection : IConnection
 {
     /// <inheritdoc />
-    public void BeginReceive(CancellationToken cancellationToken = default)
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public void BeginReceive(System.Threading.CancellationToken cancellationToken = default)
     {
         System.ObjectDisposedException.ThrowIf(_disposed, nameof(Connection));
 
-        using CancellationTokenSource linkedCts = CancellationTokenSource
+        using var linkedCts = System.Threading.CancellationTokenSource
             .CreateLinkedTokenSource(cancellationToken, _ctokens.Token);
 
         _cstream.BeginReceive(linkedCts.Token);
     }
 
     /// <inheritdoc />
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public bool Send(System.ReadOnlySpan<byte> message)
     {
         if (_cstream.Send(message))
@@ -34,17 +34,24 @@ public sealed partial class Connection : IConnection
     }
 
     /// <inheritdoc />
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public bool Send(IPacket packet) => Send(packet.Serialize().Span);
 
     /// <inheritdoc />
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public async Task<bool> SendAsync(IPacket packet, CancellationToken cancellationToken = default)
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public async System.Threading.Tasks.Task<bool> SendAsync(
+        IPacket packet,
+        System.Threading.CancellationToken cancellationToken = default)
         => await SendAsync(packet.Serialize(), cancellationToken);
 
     /// <inheritdoc />
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public async Task<bool> SendAsync(System.ReadOnlyMemory<byte> message, CancellationToken cancellationToken = default)
+    [System.Runtime.CompilerServices.MethodImpl(
+        System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public async System.Threading.Tasks.Task<bool> SendAsync(
+        System.ReadOnlyMemory<byte> message,
+        System.Threading.CancellationToken cancellationToken = default)
     {
         if (await _cstream.SendAsync(message, cancellationToken))
         {
