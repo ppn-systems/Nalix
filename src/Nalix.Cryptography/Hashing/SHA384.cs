61a655a4548e411d8b60a4812cb1d00f2dc91390
phucnguyen <nguyen098xx@gmail.com>
2025-05-02 19:27:28 +0700

    0.8.03

---
+++ src/Nalix.Cryptography/Hashing/SHA384.cs
@@ -0,0 +1,9 @@
+namespace Nalix.Cryptography.Hashing;
+
+/// <summary>
+/// Provides a managed implementation of the SHA-512 hashing algorithm.
+/// </summary>
+[System.Runtime.InteropServices.ComVisible(true)]
+internal sealed class SHA384
+{
+}
