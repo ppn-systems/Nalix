cdc095fbd6eda9c1a2dc763fbfd1e67c5c2a099f
phucnguyen <nguyen098xx@gmail.com>
2025-05-18 10:49:54 +0700

    0.8.77

--- src/Nalix.Network/Listeners/Listener.Snapshot.cs
+++ src/Nalix.Network/Listeners/Listener.Snapshot.cs
@@ -1,21 +1,23 @@
+using Nalix.Network.Snapshot;
+
 namespace Nalix.Network.Listeners;
 
-public abstract partial class Listener : Snapshot.ISnapshot<Snapshot.ListenerSnapshot>
+public abstract partial class Listener : ISnapshot<ListenerSnapshot>
 {
     /// <summary>
     /// Retrieves a snapshot of the current state of the listener, including its port, listening status,
     /// disposal status, socket address, and the status of the listener's socket.
     /// </summary>
     /// <returns>
-    /// A <see cref="Snapshot.ListenerSnapshot"/> object containing the current state of the listener.
+    /// A <see cref="ListenerSnapshot"/> object containing the current state of the listener.
     /// </returns>
-    public Snapshot.ListenerSnapshot GetSnapshot()
+    public ListenerSnapshot GetSnapshot()
         => new()
         {
             Port = _port,
             IsDisposed = _isDisposed,
             IsListening = IsListening,
-            Address = Snapshot.ListenerSnapshot.GetIpAddress(_listenerSocket),
-            ListenerSocketStatus = Snapshot.ListenerSnapshot.GetSocketStatus(_listenerSocket)
+            Address = ListenerSnapshot.GetIpAddress(_listenerSocket),
+            ListenerSocketStatus = ListenerSnapshot.GetSocketStatus(_listenerSocket)
         };
 }
