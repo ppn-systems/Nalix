d51d2891823cbb4ae318a21ee7688b3ee7170efb
phucnguyen <nguyen098xx@gmail.com>
2025-05-27 21:21:13 +0700

    0.9.55

--- src/Nalix.Shared/Contracts/NetJsonCxt.cs
+++
@@ -1,9 +0,0 @@
-namespace Nalix.Shared.Contracts;
-
-/// <summary>
-/// Provides a JSON serialization context for types in the Notio network dispatcher.
-/// </summary>
-[System.Text.Json.Serialization.JsonSerializable(typeof(PingInfoDto))]
-[System.Text.Json.Serialization.JsonSerializable(typeof(ConnInfoDto))]
-public partial class NetJsonCxt : System.Text.Json.Serialization.JsonSerializerContext
-{ }
