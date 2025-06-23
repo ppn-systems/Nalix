90e783cd6da1a41decb3b2ad89617c169af7eb0f
phucnguyen <nguyen098xx@gmail.com>
2025-06-23 16:35:18 +0700

    11.0.6
    Update test files to match recent refactoring in the main codebase:
    - Replace old Enums and Attributes namespaces with new Security ones.
    - Update messaging/packet usings to new networking/registry namespaces.
    - Switch from PacketCatalog to PacketRegistry in tests.
    - Update all references to Directories to use the new namespace/class.
    - Remove Nalix.Network.Console from the solution.
    - No changes to test logic; only namespace/class alignment.

--- tests/Nalix.Shared.Tests/Cryptography/AeadEngineTests.cs
+++ tests/Nalix.Shared.Tests/Cryptography/AeadEngineTests.cs
@@ -1,6 +1,6 @@
 // Copyright (c) 2025-2026 PPN Corporation. All rights reserved.
 
-using Nalix.Common.Enums;
+using Nalix.Common.Security.Enums;
 using Nalix.Shared.Security.Aead;
 using Nalix.Shared.Security.Engine;
 using System;
