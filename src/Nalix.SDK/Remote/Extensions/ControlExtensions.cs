c75a95db634d487218bf4e81dadde58c03c48222
phucnguyen <nguyen098xx@gmail.com>
2025-06-30 02:00:00 +0700

    8.3.5
    Migrated `IReliableClient` and `ITransportOptions` interfaces
    from `Nalix.Common.SDK` to `Nalix.Common.Client` namespace to
    improve modularity and abstraction. Updated `using` directives
    across multiple files to reflect the new namespace.
    
    Refactored `ReliableClient` to use the `ITransportOptions`
    interface instead of the `TransportOptions` concrete class.
    Removed the `Incoming` property and its related logic.
    
    Cleaned up `UnreliableClient` to remove references to the
    now-removed `Incoming` property. Adjusted `TransportOptions.cs`
    to align with the namespace changes.
    
    Duplicated copyright headers in the new interface files,
    possibly due to oversight during migration.

--- src/Nalix.SDK/Remote/Extensions/ControlExtensions.cs
+++ src/Nalix.SDK/Remote/Extensions/ControlExtensions.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
 
+using Nalix.Common.Client;
 using Nalix.Common.Packets.Abstractions;
 using Nalix.Common.Protocols;           // ControlType, ProtocolType
-using Nalix.Common.SDK;
 using Nalix.Framework.Time;             // Clock
 using Nalix.Shared.Messaging.Controls;  // Control
 
