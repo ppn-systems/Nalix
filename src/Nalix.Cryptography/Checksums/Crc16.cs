78ef644e3e13fb1a24b750f6ee2aba1c994f36c4
phucnguyen <nguyen098xx@gmail.com>
2025-07-04 12:40:45 +0700

    1.4.5

--- src/Nalix.Cryptography/Checksums/Crc16.cs
+++ src/Nalix.Cryptography/Checksums/Crc16.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -11,14 +10,24 @@ namespace Nalix.Cryptography.Checksums;
 /// </summary>
 public static class Crc16
 {
-    private const ushort Polynomial = 0x8005;
-    private const ushort InitialValue = 0xFFFF;
+    #region Const
+
+    private const System.UInt16 Polynomial = 0x8005;
+    private const System.UInt16 InitialValue = 0xFFFF;
+
+    #endregion Const
+
+    #region Fields
 
     /// <summary>
     /// Precomputed lookup table for CRC-16/MODBUS polynomial (0x8005).
     /// This table is used to speed up CRC-16 calculations.
     /// </summary>
-    private static readonly ushort[] Crc16LookupTable = Crc00.GenerateTable16(Polynomial);
+    private static readonly System.UInt16[] Crc16LookupTable = Crc00.GenerateTable16(Polynomial);
+
+    #endregion Fields
+
+    #region APIs
 
     /// <summary>
     /// Calculates the CRC16 for the entire byte array provided.
@@ -26,10 +35,10 @@ public static class Crc16
     /// <param name="bytes">The input byte array.</param>
     /// <returns>The CRC16 value as a ushort.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ushort Compute(params byte[] bytes)
+    public static System.UInt16 Compute(params System.Byte[] bytes)
     {
-        ArgumentNullException.ThrowIfNull(bytes);
-        return Compute(bytes.AsSpan());
+        System.ArgumentNullException.ThrowIfNull(bytes);
+        return Compute(System.MemoryExtensions.AsSpan(bytes));
     }
 
     /// <summary>
@@ -40,20 +49,20 @@ public static class Crc16
     /// <param name="length">The Number of bytes to process.</param>
     /// <returns>The CRC16 value as a ushort.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ushort Compute(byte[] bytes, int start, int length)
+    public static System.UInt16 Compute(System.Byte[] bytes, System.Int32 start, System.Int32 length)
     {
-        ArgumentNullException.ThrowIfNull(bytes);
+        System.ArgumentNullException.ThrowIfNull(bytes);
 
         if (bytes.Length == 0)
-            throw new ArgumentOutOfRangeException(nameof(bytes), "Byte array cannot be empty.");
+            throw new System.ArgumentOutOfRangeException(nameof(bytes), "Byte array cannot be empty.");
 
         if (start < 0 || start >= bytes.Length)
-            throw new ArgumentOutOfRangeException(nameof(start));
+            throw new System.ArgumentOutOfRangeException(nameof(start));
 
         if (length < 0 || start + length > bytes.Length)
-            throw new ArgumentOutOfRangeException(nameof(length));
+            throw new System.ArgumentOutOfRangeException(nameof(length));
 
-        return Compute(bytes.AsSpan(start, length));
+        return Compute(System.MemoryExtensions.AsSpan(bytes, start, length));
     }
 
     /// <summary>
@@ -62,10 +71,10 @@ public static class Crc16
     /// <param name="bytes">Span of input bytes.</param>
     /// <returns>CRC16 value as ushort.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ushort Compute(ReadOnlySpan<byte> bytes)
+    public static System.UInt16 Compute(System.ReadOnlySpan<System.Byte> bytes)
     {
         if (bytes.IsEmpty)
-            throw new ArgumentException("Byte span cannot be empty", nameof(bytes));
+            throw new System.ArgumentException("Byte span cannot be empty", nameof(bytes));
 
         if (Sse42.IsSupported && bytes.Length >= 8) return ComputeSse42(bytes);
         if (Vector.IsHardwareAccelerated && bytes.Length >= 16) return ComputeSimd(bytes);
@@ -79,13 +88,13 @@ public static class Crc16
     /// <param name="data">The data to compute the CRC16 for</param>
     /// <returns>The CRC16 value as a ushort</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ushort Compute<T>(ReadOnlySpan<T> data) where T : unmanaged
+    public static System.UInt16 Compute<T>(System.ReadOnlySpan<T> data) where T : unmanaged
     {
         if (data.IsEmpty)
-            throw new ArgumentException("Data span cannot be empty", nameof(data));
+            throw new System.ArgumentException("Data span cannot be empty", nameof(data));
 
         // Reinterpret the generic type as a byte span
-        ReadOnlySpan<byte> bytes = MemoryMarshal.AsBytes(data);
+        System.ReadOnlySpan<System.Byte> bytes = MemoryMarshal.AsBytes(data);
         return Compute(bytes);
     }
 
@@ -96,57 +105,63 @@ public static class Crc16
     /// <param name="expectedCrc">The expected CRC16 value</param>
     /// <returns>True if the CRC matches, otherwise false</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static bool Verify(ReadOnlySpan<byte> data, ushort expectedCrc)
+    public static bool Verify(System.ReadOnlySpan<System.Byte> data, System.UInt16 expectedCrc)
         => Compute(data) == expectedCrc;
+
+    #endregion APIs
+
+    #region Lookup Table Generation
 
     /// <summary>
     /// Processes 8 bytes at once for improved performance on larger inputs.
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static ushort ProcessOctet(ushort crc, ReadOnlySpan<byte> octet)
+    private static System.UInt16 ProcessOctet(
+        System.UInt16 crc,
+        System.ReadOnlySpan<System.Byte> octet)
     {
-        ref byte data = ref MemoryMarshal.GetReference(octet);
+        ref System.Byte data = ref MemoryMarshal.GetReference(octet);
 
-        crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ data) & 0xFF]);
-        crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 1)) & 0xFF]);
-        crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 2)) & 0xFF]);
-        crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 3)) & 0xFF]);
-        crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 4)) & 0xFF]);
-        crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 5)) & 0xFF]);
-        crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 6)) & 0xFF]);
-        crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 7)) & 0xFF]);
+        crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ data) & 0xFF]);
+        crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 1)) & 0xFF]);
+        crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 2)) & 0xFF]);
+        crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 3)) & 0xFF]);
+        crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 4)) & 0xFF]);
+        crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 5)) & 0xFF]);
+        crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 6)) & 0xFF]);
+        crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref data, 7)) & 0xFF]);
 
         return crc;
     }
 
-    private static ushort ComputeScalar(ReadOnlySpan<byte> bytes)
+    private static System.UInt16 ComputeScalar(System.ReadOnlySpan<byte> bytes)
     {
-        ushort crc = InitialValue;
+        System.UInt16 crc = InitialValue;
 
         // Process 8 bytes at once for larger inputs
         if (bytes.Length >= 8)
         {
-            int blockCount = bytes.Length / 8;
-            int remaining = bytes.Length % 8;
+            System.Int32 blockCount = bytes.Length / 8;
+            System.Int32 remaining = bytes.Length % 8;
 
             // Process 8-byte blocks
-            for (int i = 0; i < blockCount * 8; i += 8)
+            for (System.Int32 i = 0; i < blockCount * 8; i += 8)
             {
                 crc = ProcessOctet(crc, bytes.Slice(i, 8));
             }
 
             // Process remaining bytes
-            for (int i = bytes.Length - remaining; i < bytes.Length; i++)
+            for (System.Int32 i = bytes.Length - remaining; i < bytes.Length; i++)
             {
-                crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ bytes[i]) & 0xFF]);
+                crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ bytes[i]) & 0xFF]);
             }
         }
         else
         {
             // For small inputs, use the simple loop
-            for (int i = 0; i < bytes.Length; i++)
+            for (System.Int32 i = 0; i < bytes.Length; i++)
             {
-                crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ bytes[i]) & 0xFF]);
+                crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ bytes[i]) & 0xFF]);
             }
         }
 
@@ -154,24 +169,24 @@ public static class Crc16
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static ushort ComputeSse42(ReadOnlySpan<byte> bytes)
+    private static System.UInt16 ComputeSse42(System.ReadOnlySpan<System.Byte> bytes)
     {
-        ushort crc = InitialValue;
+        System.UInt16 crc = InitialValue;
 
         if (Sse42.IsSupported)
         {
-            ref byte start = ref MemoryMarshal.GetReference(bytes);
-            int length = bytes.Length;
+            ref System.Byte start = ref MemoryMarshal.GetReference(bytes);
+            System.Int32 length = bytes.Length;
 
-            int i = 0;
+            System.Int32 i = 0;
             for (; i + 8 <= length; i += 8)
             {
-                crc = (ushort)Sse42.Crc32(crc, Unsafe.ReadUnaligned<uint>(ref Unsafe.Add(ref start, i)));
+                crc = (System.UInt16)Sse42.Crc32(crc, Unsafe.ReadUnaligned<System.UInt32>(ref Unsafe.Add(ref start, i)));
             }
 
             for (; i < length; i++)
             {
-                crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref start, i)) & 0xFF]);
+                crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref start, i)) & 0xFF]);
             }
         }
         else
@@ -183,26 +198,26 @@ public static class Crc16
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static ushort ComputeSimd(ReadOnlySpan<byte> bytes)
+    private static System.UInt16 ComputeSimd(System.ReadOnlySpan<System.Byte> bytes)
     {
-        ushort crc = InitialValue;
-        int vectorSize = Vector<byte>.Count;
-        int vectorCount = bytes.Length / vectorSize;
+        System.UInt16 crc = InitialValue;
+        System.Int32 vectorSize = Vector<System.Byte>.Count;
+        System.Int32 vectorCount = bytes.Length / vectorSize;
 
         if (vectorCount > 0)
         {
-            ref byte start = ref MemoryMarshal.GetReference(bytes);
-            int i = 0;
+            ref System.Byte start = ref MemoryMarshal.GetReference(bytes);
+            System.Int32 i = 0;
 
             for (; i < vectorCount * vectorSize; i += vectorSize)
             {
-                Vector<byte> vec = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.Add(ref start, i));
+                Vector<System.Byte> vec = Unsafe.ReadUnaligned<Vector<System.Byte>>(ref Unsafe.Add(ref start, i));
                 crc = ProcessVector(crc, vec);
             }
 
             for (; i < bytes.Length; i++)
             {
-                crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref start, i)) & 0xFF]);
+                crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ Unsafe.Add(ref start, i)) & 0xFF]);
             }
         }
         else
@@ -214,12 +229,14 @@ public static class Crc16
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static ushort ProcessVector(ushort crc, Vector<byte> vec)
+    private static System.UInt16 ProcessVector(System.UInt16 crc, Vector<System.Byte> vec)
     {
-        for (int i = 0; i < Vector<byte>.Count; i++)
+        for (int i = 0; i < Vector<System.Byte>.Count; i++)
         {
-            crc = (ushort)(crc >> 8 ^ Crc16LookupTable[(crc ^ vec[i]) & 0xFF]);
+            crc = (System.UInt16)((crc >> 8) ^ Crc16LookupTable[(crc ^ vec[i]) & 0xFF]);
         }
         return crc;
     }
+
+    #endregion Lookup Table Generation
 }
