bef2768e17b30b2222371f26bc53b1e375ac02d1
phucnguyen <nguyen098xx@gmail.com>
2025-07-12 19:39:23 +0700

    1.9.5
    Added caching and pooling capabilities to `PacketContext` and `ControllerScanner` classes to reduce allocations and improve method access efficiency. Implemented the `IPoolable` interface in `PacketContext` and introduced a `ResetForPool` method. Refactored `ControllerScanner` for better logging and error handling, and added new return type handlers like `ReadOnlyMemoryReturnHandler`, `TaskVoidReturnHandler`, and `ValueTaskVoidReturnHandler` for improved asynchronous result management. Updated `PacketDispatchOptions` to support logging and error handling configurations.

---
+++ src/Nalix.Network/Dispatch/Handlers/ReadOnlyMemoryReturnHandler.cs
@@ -0,0 +1,20 @@
+﻿using Nalix.Network.Dispatch.Core;
+using System.Runtime.CompilerServices;
+
+namespace Nalix.Network.Dispatch.Handlers;
+
+/// <inheritdoc/>
+public sealed class ReadOnlyMemoryReturnHandler<TPacket> : IReturnTypeHandler<TPacket>
+{
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public async System.Threading.Tasks.ValueTask HandleAsync(
+        System.Object? result,
+        PacketContext<TPacket> context)
+    {
+        if (result is System.ReadOnlyMemory<System.Byte> memory)
+        {
+            await context.Connection.Tcp.SendAsync(memory);
+        }
+    }
+}
