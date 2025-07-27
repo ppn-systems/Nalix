987e3eaa79ff5cf20a7d3a902a48058186a5934a
phucnguyen <nguyen098xx@gmail.com>
2025-07-27 20:02:50 +0700

    2.6.1
    - Changed namespaces for `PacketConstants` and `Timeouts` from `Nalix.Common.Constants` to `Nalix.Common.Packets`.
    - Updated `System.AttributeUsage` in several attribute files to restrict usage to a single instance per method.
    - Modified `ConnectionLimitType` enum values to hexadecimal representations.
    - Removed unnecessary `using` directives in `RemoteStreamReceiver`.
    - Introduced `PacketGuardAttribute` for enforcing security constraints on methods, along with a new `GuardType` enum for various security guard types.

---
+++ src/Nalix.Common/Packets/Attributes/PacketGuardAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Nalix.Common.Security.Types;
+using System;
+
+namespace Nalix.Common.Packets.Attributes;
+
+/// <summary>
+/// Specifies a security guard to be applied to a method, enforcing specific security constraints.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="PacketGuardAttribute"/> class with the specified guard type.
+/// </remarks>
+/// <param name="type">The type of security guard to enforce.</param>
+[System.Obsolete("This feature is not ready for public use yet.")]
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+public sealed class PacketGuardAttribute(GuardType type) : System.Attribute
+{
+    /// <summary>
+    /// Gets the type of security guard to be applied.
+    /// </summary>
+    public GuardType Type { get; } = type;
+}
