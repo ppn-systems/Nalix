56c1c8cd7d0d272b081e73a6011e658bc4ed8496
phucnguyen <nguyen098xx@gmail.com>
2025-08-16 08:28:22 +0700

    3.5.5
    - Refactored `MagicNumbers` enum to include new values for `Control`, `Text256`, `Text512`, and `Text1024`.
    - Added `MonoTicksNow()` method in `Clock.cs` for monotonic tick count.
    - Updated `PacketRegistry.cs` with new using directives and modified static constructor for assembly scanning.
    - Refactored `Handshake` class to use `Control` magic number and updated serialization methods.
    - Introduced new `Control` class for transmitting control messages with relevant properties and methods.
    - Added `ControlType` enum for various control message types.
    - Updated `Nalix.Shared.csproj` to reference `Nalix.Framework`.
    - Modified `Text512` class to use the correct magic number.

--- src/Nalix.Common/Attributes/MagicNumbers.cs
+++ src/Nalix.Common/Attributes/MagicNumbers.cs
@@ -18,40 +18,45 @@ public enum MagicNumbers : System.UInt32
     /// <summary>
     /// Represents the handshake process used to establish a connection or agreement between two parties.
     /// </summary>
-    Handshake,
+    Handshake = 0x0000A001,
+
+    /// <summary>
+    /// Control packet used for signaling or managing protocol state.
+    /// </summary>
+    Control = 0x0000A009,
 
     /// <summary>
     /// Binary data packet with a maximum payload size of 128 bytes.
     /// </summary>
-    Binary128,
+    Binary128 = 0x0000A002,
 
     /// <summary>
     /// Binary data packet with a maximum payload size of 256 bytes.
     /// </summary>
-    Binary256,
+    Binary256 = 0x0000A003,
 
     /// <summary>
     /// Binary data packet with a maximum payload size of 512 bytes.
     /// </summary>
-    Binary512,
+    Binary512 = 0x0000A004,
 
     /// <summary>
     /// Binary data packet with a maximum payload size of 1024 bytes.
     /// </summary>
-    Binary1024,
+    Binary1024 = 0x0000A005,
 
     /// <summary>
     /// UTF-8 text packet with a maximum payload size of 256 bytes.
     /// </summary>
-    Text256,
+    Text256 = 0x0000A006,
 
     /// <summary>
     /// UTF-8 text packet with a maximum payload size of 512 bytes.
     /// </summary>
-    Text512,
+    Text512 = 0x0000A007,
 
     /// <summary>
     /// UTF-8 text packet with a maximum payload size of 1024 bytes.
     /// </summary>
-    Text1024
+    Text1024 = 0x0000A008
 }
