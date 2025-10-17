1596f6e4e3ba3d34d3a44c411671e49c7a778857
phucnguyen <nguyen098xx@gmail.com>
2025-10-18 02:00:00 +0700

    11.0.0
    Reorganized namespaces and using directives across the codebase:
    - Moved enums from Packets.Core to Packets.Enums.
    - Split serialization attributes/interfaces into dedicated namespaces.
    - Updated all affected files to use new namespaces.
    - Removed redundant copyright/license comments.
    No functional changes; improves maintainability and code hygiene.

--- src/Nalix.Common/Serialization/Attributes/SerializePackableAttribute.cs
+++ src/Nalix.Common/Serialization/Attributes/SerializePackableAttribute.cs
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 PPN Corporation. All rights reserved.
 // Licensed under the Apache License, Version 2.0.
 
-namespace Nalix.Common.Serialization;
+namespace Nalix.Common.Serialization.Attributes;
 
 /// <summary>
 /// An attribute that marks a class, struct, or interface as serializable with a specified layout.
