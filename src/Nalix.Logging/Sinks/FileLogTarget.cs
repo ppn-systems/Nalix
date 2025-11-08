4963a3d1daa3999d31540c6f5bdc9e7e1d5fd889
phucnguyen <nguyen098xx@gmail.com>
2025-11-08 23:50:51 +0700

    7.3.0
    Updated ConsoleLogTarget.cs and FileLogTarget.cs to include a new `using` directive for the `Nalix.Logging.Core` namespace. This change enables the use of additional functionality or types from the `Nalix.Logging.Core` namespace, enhancing the modularity and capabilities of the logging framework.

--- src/Nalix.Logging/Sinks/FileLogTarget.cs
+++ src/Nalix.Logging/Sinks/FileLogTarget.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2025 PPN Corporation. All rights reserved.
 
 using Nalix.Common.Logging;
+using Nalix.Logging.Core;
 using Nalix.Logging.Internal.File;
 using Nalix.Logging.Options;
 
