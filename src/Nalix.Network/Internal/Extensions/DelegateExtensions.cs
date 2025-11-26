33474023df7a35db68bbe0a18d0509a234dbf41b
phucnguyen <nguyen098xx@gmail.com>
2025-11-26 17:19:13 +0700

    8.4.6
    Updated logging in `HandlerCompiler.cs` to improve clarity and provide more detailed information about controllers and compiled methods. Added a debug log for cases where no methods are found.
    
    Replaced `using` directives in `DelegateExtensions.cs` with fully qualified namespaces to enhance code clarity and avoid ambiguity. Refactored LINQ usage to explicitly use `System.Linq.Enumerable` methods. Improved exception messages and comments for better debugging and documentation.
    
    Simplified `NetTaskCatalog.cs` by removing an unused `#if DEBUG` directive while retaining `InternalsVisibleTo` attributes for test and benchmark assemblies.
    
    Added or updated copyright notices for 2025 PPN Corporation across modified files.

--- src/Nalix.Network/Internal/Extensions/DelegateExtensions.cs
+++ src/Nalix.Network/Internal/Extensions/DelegateExtensions.cs
@@ -1,26 +1,24 @@
-﻿using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
+﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
 
 namespace Nalix.Network.Internal.Extensions;
 
 /// <summary>
 /// Provides helper methods to construct open delegate types (Func/Action)
-/// that match a target <see cref="MethodInfo"/> signature.
+/// that match a target <see cref="System.Reflection.MethodInfo"/> signature.
 /// </summary>
 internal static class DelegateExtensions
 {
     /// <summary>
     /// Builds a delegate <see cref="System.Type"/> for a static method, matching its parameter list
-    /// and return type. Uses <see cref="Expression.GetActionType(System.Type[])"/> for <c>void</c> and
-    /// <see cref="Expression.GetDelegateType(System.Type[])"/> otherwise.
+    /// and return type. Uses <see cref="System.Linq.Expressions.Expression.GetActionType(System.Type[])"/> for <c>void</c> and
+    /// <see cref="System.Linq.Expressions.Expression.GetDelegateType(System.Type[])"/> otherwise.
     /// </summary>
     /// <param name="mi">The static method metadata.</param>
     /// <returns>A <see cref="System.Type"/> representing an <c>Action&lt;...&gt;</c> or <c>Func&lt;...&gt;</c> shape.</returns>
     /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="mi"/> is null.</exception>
     /// <exception cref="System.ArgumentException">Thrown when the method is not static.</exception>
     /// <exception cref="System.NotSupportedException">Thrown for methods with ref/out parameters or too many parameters.</exception>
-    internal static System.Type CreateDelegateTypeForStatic(this MethodInfo mi)
+    public static System.Type CreateDelegateTypeForStatic(this System.Reflection.MethodInfo mi)
     {
         System.ArgumentNullException.ThrowIfNull(mi);
 
@@ -29,44 +27,45 @@ internal static class DelegateExtensions
             throw new System.ArgumentException("MethodInfo must be static.", nameof(mi));
         }
 
-        var paramTypes = mi.GetParameters();
+        System.Reflection.ParameterInfo[] paramTypes = mi.GetParameters();
 
         // Disallow ref/out for this generic delegate approach.
-        if (paramTypes.Any(p => p.ParameterType.IsByRef))
+        if (System.Linq.Enumerable.Any(paramTypes, p => p.ParameterType.IsByRef))
         {
             throw new System.NotSupportedException("Ref/out parameters are not supported for generated delegate type.");
         }
 
         // Build the type list: [p1, p2, ..., (return?)]
-        var types = paramTypes.Select(p => p.ParameterType).ToList();
+        System.Collections.Generic.List<System.Type> types = System.Linq.Enumerable.ToList(System.Linq.Enumerable
+                                                                                   .Select(paramTypes, p => p.ParameterType));
 
         // Expression.GetActionType/GetDelegateType limit: up to 16 parameters (Action) / 17 including return (Func).
         // This is more than enough for handlers here.
         if (mi.ReturnType == typeof(void))
         {
             EnsureParameterLimit(types.Count, isVoid: true);
-            return Expression.GetActionType([.. types]);
+            return System.Linq.Expressions.Expression.GetActionType([.. types]);
         }
         else
         {
             EnsureParameterLimit(types.Count + 1, isVoid: false); // +1 for return type
             types.Add(mi.ReturnType);
-            return Expression.GetDelegateType([.. types]);
+            return System.Linq.Expressions.Expression.GetDelegateType([.. types]);
         }
     }
 
     /// <summary>
     /// Builds a delegate <see cref="System.Type"/> for an instance method as an open-instance delegate,
     /// i.e., the first parameter is the declaring instance (<c>this</c>), followed by the method parameters.
-    /// Uses <see cref="Expression.GetActionType(System.Type[])"/> for <c>void</c> and
-    /// <see cref="Expression.GetDelegateType(System.Type[])"/> otherwise.
+    /// Uses <see cref="System.Linq.Expressions.Expression.GetActionType(System.Type[])"/> for <c>void</c> and
+    /// <see cref="System.Linq.Expressions.Expression.GetDelegateType(System.Type[])"/> otherwise.
     /// </summary>
     /// <param name="mi">The instance method metadata.</param>
     /// <returns>A <see cref="System.Type"/> representing an open-instance <c>Action&lt;TDeclaring,...&gt;</c> or <c>Func&lt;TDeclaring,...,TReturn&gt;</c>.</returns>
     /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="mi"/> is null.</exception>
     /// <exception cref="System.ArgumentException">Thrown when the method is static or has no declaring type.</exception>
     /// <exception cref="System.NotSupportedException">Thrown for methods with ref/out parameters or too many parameters.</exception>
-    internal static System.Type CreateDelegateTypeForInstance(this MethodInfo mi)
+    public static System.Type CreateDelegateTypeForInstance(this System.Reflection.MethodInfo mi)
     {
         System.ArgumentNullException.ThrowIfNull(mi);
 
@@ -80,36 +79,36 @@ internal static class DelegateExtensions
             throw new System.ArgumentException("MethodInfo must have a declaring type.", nameof(mi));
         }
 
-        var paramTypes = mi.GetParameters();
+        System.Reflection.ParameterInfo[] paramTypes = mi.GetParameters();
 
-        if (paramTypes.Any(p => p.ParameterType.IsByRef))
+        if (System.Linq.Enumerable.Any(paramTypes, p => p.ParameterType.IsByRef))
         {
             throw new System.NotSupportedException("Ref/out parameters are not supported for generated delegate type.");
         }
 
         // Open instance: prepend 'this' (declaring type)
-        var types = new System.Collections.Generic.List<System.Type>(1 + paramTypes.Length)
+        System.Collections.Generic.List<System.Type> types = new(1 + paramTypes.Length)
         {
             mi.DeclaringType
         };
-        types.AddRange(paramTypes.Select(p => p.ParameterType));
+        types.AddRange(System.Linq.Enumerable.Select(paramTypes, p => p.ParameterType));
 
         if (mi.ReturnType == typeof(void))
         {
             EnsureParameterLimit(types.Count, isVoid: true);
-            return Expression.GetActionType([.. types]);
+            return System.Linq.Expressions.Expression.GetActionType([.. types]);
         }
         else
         {
             EnsureParameterLimit(types.Count + 1, isVoid: false);
             types.Add(mi.ReturnType);
-            return Expression.GetDelegateType([.. types]);
+            return System.Linq.Expressions.Expression.GetDelegateType([.. types]);
         }
     }
 
     /// <summary>
-    /// Validates parameter count limits of <see cref="Expression.GetActionType(System.Type[])"/> and
-    /// <see cref="Expression.GetDelegateType(System.Type[])"/>.
+    /// Validates parameter count limits of <see cref="System.Linq.Expressions.Expression.GetActionType(System.Type[])"/> and
+    /// <see cref="System.Linq.Expressions.Expression.GetDelegateType(System.Type[])"/>.
     /// </summary>
     /// <param name="arity">
     /// For <c>Action</c>: number of parameters.
