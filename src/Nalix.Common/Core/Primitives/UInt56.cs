17da65c03a8e0d2d919073082e5558ca52adcdf2
phucnguyen <nguyen098xx@gmail.com>
2026-01-14 19:56:51 +0700

    9.8.1
    Refactored ISnowflake.cs and UInt56.cs to use the Nalix.Common.Core.Primitives namespace instead of Nalix.Common.Primitives. Updated using directives accordingly to reflect the new project structure.

--- src/Nalix.Common/Core/Primitives/UInt56.cs
+++ src/Nalix.Common/Core/Primitives/UInt56.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
 
-namespace Nalix.Common.Primitives;
+namespace Nalix.Common.Core.Primitives;
 
 /// <summary>
 /// Represents an unsigned 56-bit integer.
