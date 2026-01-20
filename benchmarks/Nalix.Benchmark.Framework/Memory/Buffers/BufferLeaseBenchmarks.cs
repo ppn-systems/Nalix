561e1447032f462f0b8eef9d66b61137c6e9a956
phucnguyen <phcnguyenz@proton.me>
2026-01-20 21:52:48 +0700

    Add comprehensive benchmark suite for core framework components
    Introduced a wide range of new benchmark classes covering BufferLease, BufferPool, Clock, Csprng, DataWriter, cryptographic engines, fragment assembly, hashing, LiteSerializer, ListPool, LZ4Codec, ObjectMap, ObjectPool, ObjectPoolManager, Snowflake, and TaskManager. Each benchmark uses BenchmarkDotNet for memory diagnostics and is designed to measure the performance of key operations in memory management, serialization, cryptography, random number generation, time utilities, and task management within the Nalix framework. This provides a solid foundation for ongoing performance analysis and optimization.

---
+++ benchmarks/Nalix.Benchmark.Framework/Memory/Buffers/BufferLeaseBenchmarks.cs
@@ -0,0 +1,41 @@
+using System;
+using BenchmarkDotNet.Attributes;
+using Nalix.Framework.Memory.Buffers;
+
+namespace Nalix.Benchmark.Framework.Memory.Buffers;
+
+[MemoryDiagnoser]
+[Config(typeof(global::Nalix.Benchmark.Framework.BenchmarkConfig))]
+public class BufferLeaseBenchmarks
+{
+    private byte[] _source = null!;
+
+    [Params(128, 2048)]
+    public int PayloadBytes { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _source = new byte[this.PayloadBytes];
+        for (int i = 0; i < _source.Length; i++)
+        {
+            _source[i] = (byte)(i % 251);
+        }
+    }
+
+    [Benchmark]
+    public int Rent_Commit_Dispose()
+    {
+        using BufferLease lease = BufferLease.Rent(this.PayloadBytes);
+        MemoryExtensions.AsSpan(_source).CopyTo(lease.SpanFull);
+        lease.CommitLength(_source.Length);
+        return lease.Length;
+    }
+
+    [Benchmark]
+    public int CopyFrom_Dispose()
+    {
+        using BufferLease lease = BufferLease.CopyFrom(_source);
+        return lease.Length;
+    }
+}
