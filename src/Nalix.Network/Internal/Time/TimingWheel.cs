29f2b90d14c9863a6f376c5ff0bb93c2d96b4cae
phucnguyen <phcnguyenz@proton.me>
2026-04-12 15:28:21 +0700

    Restrict TimingWheel class accessibility
    Changed the `TimingWheel` class from `public sealed` to `internal sealed` to limit its visibility to within the same assembly, improving encapsulation and aligning with internal usage requirements.

--- src/Nalix.Network/Internal/Time/TimingWheel.cs
+++ src/Nalix.Network/Internal/Time/TimingWheel.cs
@@ -66,7 +66,7 @@ namespace Nalix.Network.Internal.Time;
 /// <seealso cref="IActivatable"/>
 [SkipLocalsInit]
 [DebuggerNonUserCode]
-public sealed class TimingWheel : IActivatable
+internal sealed class TimingWheel : IActivatable
 {
     #region Fields
 
