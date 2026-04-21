db2fc6d7b4121be86fd5f0f63144ab80ec0ea7bc
phucnguyen <phcnguyenz@proton.me>
2026-04-21 18:25:39 +0700

    Remove InternalsVisibleTo attributes and license header
    Removed the copyright and license header from InternalsVisibleTo.cs.
    Also deleted the using directive and InternalsVisibleTo attributes,
    which revokes internal access for tests and benchmarks assemblies.

--- src/Nalix.Framework/Properties/InternalsVisibleTo.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2025-2026 PPN Corporation. All rights reserved.
-// Licensed under the Apache License, Version 2.0.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Nalix.Framework.Tests")]
-[assembly: InternalsVisibleTo("Nalix.Framework.Benchmarks")]
