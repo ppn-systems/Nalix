6fe5f32f4fd0299380b164fe99c059c8fbe5f068
phucnguyen <phcnguyenz@proton.me>
2026-05-03 20:18:51 +0700

    refactor(codec): update signal formatters

--- src/Nalix.Codec/DataFrames/Formatter/DirectiveFormatter.cs
+++ src/Nalix.Codec/DataFrames/Formatter/DirectiveFormatter.cs
@@ -1,6 +1,13 @@
 // Copyright (c) 2026 PPN Corporation. All rights reserved.
 // Licensed under the Apache License, Version 2.0.
 
+/* 
+ * PERFORMANCE NOTE:
+ * This formatter is pre-registered in the static constructor of the Control class 
+ * to eliminate runtime lookups and bypass dynamic delegate loops. 
+ * This ensures maximum throughput and zero-allocation during serialization.
+ */
+
 using System.Runtime.CompilerServices;
 using Nalix.Abstractions.Networking.Protocols;
 using Nalix.Abstractions.Primitives;
