6f482a8fdc172bcdd9277b5a7db10797000d4048
phucnguyen <phcnguyenz@proton.me>
2026-05-25 23:49:34 +0700

    fix(hosting): avoid double disposing invalid frames

--- src/Nalix.Hosting/Internal/WebSocketServerListener.cs
+++ src/Nalix.Hosting/Internal/WebSocketServerListener.cs
@@ -47,7 +47,6 @@ internal sealed class WebSocketServerListener : WebSocketListenerBase
 
             if (!args.Connection.TCP.ReceiveSequence.IsValid(seq, window: this.SequenceOptions.TcpWindow))
             {
-                current.Dispose();
                 return;
             }
 
