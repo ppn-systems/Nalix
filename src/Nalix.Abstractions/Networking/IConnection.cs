b13acb76b21c5f7b2924e30845a171a0efa6ba44
phucnguyen <phcnguyenz@proton.me>
2026-06-12 22:42:55 +0700

    refactor: update IConnection and SingletonBase

--- src/Nalix.Abstractions/Networking/IConnection.cs
+++ src/Nalix.Abstractions/Networking/IConnection.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.ComponentModel;
-using Nalix.Abstractions.Identity;
 using Nalix.Abstractions.Networking.Protocols;
 using Nalix.Abstractions.Primitives;
 using Nalix.Abstractions.Security;
