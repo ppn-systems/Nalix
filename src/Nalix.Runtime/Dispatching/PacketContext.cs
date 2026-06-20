ce4302f800593a04d6abc4c3f5828a77d73a4793
phucnguyen <phcnguyenz@proton.me>
2026-06-20 19:40:13 +0700

    refactor: PacketRegistry, NetworkApplicationBuilder, and Network options
    - Update PacketRegistry codec
    - Refactor NetworkApplicationBuilder hosting
    - Update NetworkPoolInitializer
    - Refactor UdpListener public methods
    - Update NetworkSocketOptions
    - Improve PacketContext dispatch

--- src/Nalix.Runtime/Dispatching/PacketContext.cs
+++ src/Nalix.Runtime/Dispatching/PacketContext.cs
@@ -103,8 +103,8 @@ public sealed class PacketContext<TPacket> : IPacketContext<TPacket>, IPoolable,
 
     static PacketContext()
     {
-        _ = s_pool.SetMaxCapacity<PacketContext<TPacket>>(256);
-        _ = s_pool.Prealloc<PacketContext<TPacket>>(256);
+        _ = s_pool.SetMaxCapacity<PacketContext<TPacket>>(128);
+        _ = s_pool.Prealloc<PacketContext<TPacket>>(128);
     }
 
     /// <summary>
